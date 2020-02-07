8848762dbc1d7bdc865c454762c10a2504e59625
Giulio Romualdi <giulio.romualdi@gmail.com>
2020-02-07 17:21:25 +0100

    Fix Typos in YarpImplementation/tests for the ParametersHandler library

--- src/ParametersHandler/YarpImplementation/tests/ParametersHandlerYarpTest.cpp
+++ src/ParametersHandler/YarpImplementation/tests/ParametersHandlerYarpTest.cpp
@@ -1,5 +1,5 @@
 /**
- * @file ParametesHandlerYarpTest.cpp
+ * @file ParametersHandlerYarpTest.cpp
  * @authors Giulio Romualdi
  * @copyright 2020 Istituto Italiano di Tecnologia (IIT). This software may be modified and
  * distributed under the terms of the GNU Lesser General Public License v2.1 or any later version.
