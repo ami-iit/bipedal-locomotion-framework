044d937f741869897f1826c4c7ac0d03c8d931fc
Giulio Romualdi <giulio.romualdi@gmail.com>
2020-02-11 16:25:11 +0100

    - Move the ContactWrenchFeasibilityElement in ContactWrenchFeasibilityElement.* - Implement ContactWrenchRateOfChangeFeasibilityElement class

--- src/OptimalControlUtilities/include/BipedalLocomotionControllers/OptimalControlUtilities/FeasibilityElements.h
+++ src/OptimalControlUtilities/include/BipedalLocomotionControllers/OptimalControlUtilities/FeasibilityElements.h
@@ -16,65 +16,6 @@ namespace BipedalLocomotionControllers
 
 namespace OptimalControlUtilities
 {
-
-/**
- * ContactWrenchFeasibilityElement handles the constraints related to the contact wrenches
- * such that: Unilateral constraint, friction cone, torsional friction parameter
- * and position of the local Center of Pressure
- */
-class ContactWrenchFeasibilityElement : public InequalityConstraintElement
-{
-    Frame<iDynTree::IndexRange, iDynTree::FrameIndex> m_frameInContact; /**< Frame in contact with the environment */
-    iDynTree::Rotation m_rotationMatrix; /**< Frame rotation matrix */
-    iDynTree::MatrixDynSize m_AInBodyFrame; /**< Constrain matrix written in body frame */
-    double m_infinity; /**< Double representing the infinity */
-    double m_minimalNormalForce; /**< Minimal normal force required */
-    unsigned int m_nominalForceConstraintIndex; /**< Index of the minimal normal force required */
-
-public:
-    /**
-     * Constructor.
-     * @param kinDyn an iDynTree kinDyn computation object
-     * @param handler the variable handler object
-     * @param frameInContact pair containing the frames in contact. The first
-     * element is the name of the frame in the handler while the second is
-     * the name in the model
-     * @param numberOfPoints number of points used to approximate a quarter of the friction cone
-     * @param staticFrictionCoefficient static friction coefficient
-     * @param torsionalFrictionCoefficient torsional friction coefficient
-     * @param minimalNormalForce minimal normal force required
-     * @param footLimitX foot size on the X axis written with respect the foot frame. The
-     * first value should be negative while the second one positive
-     * @param footLimitY foot size on the Y axis written with respect the foot frame. The
-     * first value should be negative while the second one positive
-     * @param infinity double representing the infinity
-     * @throw std::runtime_error if the frame is not defined
-     */
-    ContactWrenchFeasibilityElement(std::shared_ptr<iDynTree::KinDynComputations> kinDyn,
-                                    const VariableHandler& handler,
-                                    const Frame<std::string, std::string>& frameInContact,
-                                    const int& numberOfPoints,
-                                    const double& staticFrictionCoefficient,
-                                    const double& torsionalFrictionCoefficient,
-                                    const double& minimalNormalForce,
-                                    const iDynTree::Vector2& footLimitX,
-                                    const iDynTree::Vector2& footLimitY,
-                                    const double& infinity);
-
-    /**
-     * Set if the link is in contact with the environment
-     * @param isInContact true if the link associated to the frame is in contact with the
-     * environment
-     */
-    void isInContact(bool isInContact);
-
-    /**
-     * Get (and compute) the element matrix
-     * @return the element matrix
-     */
-    virtual const iDynTree::MatrixDynSize& getA() final;
-};
-
 /**
  * JointValuesFeasibilityElement handles the robot joint limits
  */
