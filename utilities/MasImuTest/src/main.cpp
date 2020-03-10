f0f8d2a3822e1a7f05df82bea98ebde8a7c833fd
Stefano <stefano.dafarra@gmail.com>
2020-03-10 17:29:09 +0100

    Updated main of MasImuTest.

--- utilities/MasImuTest/src/main.cpp
+++ utilities/MasImuTest/src/main.cpp
@@ -5,10 +5,31 @@
  * distributed under the terms of the GNU Lesser General Public License v2.1 or any later version.
  */
 
+// YARP
+#include <yarp/os/LogStream.h>
+#include <yarp/os/Network.h>
+#include <yarp/os/RFModule.h>
+
 #include <BipedalLocomotionControllers/MasImuTest.h>
 
 int main(int argc, char * argv[])
 {
+    // initialise yarp network
+    yarp::os::Network yarp;
+    if (!yarp.checkNetwork())
+    {
+        yError()<<"[main] Unable to find YARP network";
+        return EXIT_FAILURE;
+    }
+
+    // prepare and configure the resource finder
+    yarp::os::ResourceFinder& rf = yarp::os::ResourceFinder::getResourceFinderSingleton();
+
+    rf.setDefaultConfigFile("dcm_walking_with_joypad.ini");
+
+    rf.configure(argc, argv);
+
     BipedalLocomotionControllers::MasImuTest test;
 
+    return test.runModule(rf);
 }
