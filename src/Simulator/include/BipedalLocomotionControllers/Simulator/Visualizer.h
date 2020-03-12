a63e917902695752fe6b368ca15d4ad7e80da0fb
Giulio Romualdi <giulio.romualdi@gmail.com>
2020-03-12 18:44:17 +0100

    Improve documentation in Visualizer.h

--- src/Simulator/include/BipedalLocomotionControllers/Simulator/Visualizer.h
+++ src/Simulator/include/BipedalLocomotionControllers/Simulator/Visualizer.h
@@ -18,29 +18,70 @@ namespace BipedalLocomotionControllers
 {
 namespace Simulator
 {
+/**
+ * Handler for the iDynTree visualizer
+ */
 class Visualizer
 {
     struct Impl;
-    std::unique_ptr<Impl> m_pimpl;
+    std::unique_ptr<Impl> m_pimpl; /**< Private implementation */
 
 public:
+    /**
+     * Constructor
+     */
     Visualizer();
 
+    /**
+     * Destructor
+     */
     ~Visualizer();
 
+    /**
+     * Add a new model to the visualizer
+     * @param model reference to the model
+     * @param modelName name of the model
+     * @return true/false in case of success/failure
+     */
     bool addModel(const iDynTree::Model& model, const std::string& modelName);
 
+    /**
+     * Visualize the state
+     * @param world_T_Base homogeneous transformation of the base w.r.t. the inertial frame
+     * @param jointPosition position of the joints
+     * @param contactWrenches vector containing the contact wrenches. A contact wrench is defined by
+     * an homogeneous transformation and a wrench
+     * @return true/false in case of success/failure
+     */
     bool visualizeState(const iDynTree::Transform& world_T_Base,
                         const iDynTree::VectorDynSize& jointsPosition,
                         const std::vector<std::pair<iDynTree::Transform, iDynTree::Wrench>>& contactWrenches);
 
-
+    /**
+     * Set the position of the camera
+     * @param cameraPosition position of the camera
+     * @return true/false in case of success/failure
+     */
     bool setCameraPosition(const iDynTree::Position& cameraPosition);
 
+    /**
+     * Set the target of the camera
+     * @param cameraTarget position of the target
+     * @return true/false in case of success/failure
+     */
     bool setCameraTarget(const iDynTree::Position& cameraTarget);
 
+    /**
+     * Set the light direction
+     * @param lightDirection direction of the light
+     * @return true/false in case of success/failure
+     */
     bool setLightDirection(const iDynTree::Direction& lightDirection);
 
+    /**
+     * Salve a frame as png image
+     * @return true/false in case of success/failure
+     */
     bool saveFrame();
 };
 } // namespace Simulator
