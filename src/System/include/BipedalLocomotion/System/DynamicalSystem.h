fe007b71db9317a9f8b6b93455bb0961fd49bf37
Giulio Romualdi <giulio.romualdi@gmail.com>
2020-05-20 22:09:23 +0200

    Implement the first version of the dynamical system class

---
+++ src/System/include/BipedalLocomotion/System/DynamicalSystem.h
@@ -0,0 +1,93 @@
+/**
+ * @file DynamicalSystem.h
+ * @authors Giulio Romualdi
+ * @copyright 2020 Istituto Italiano di Tecnologia (IIT). This software may be modified and
+ * distributed under the terms of the GNU Lesser General Public License v2.1 or any later version.
+ */
+
+#ifndef BIPEDAL_LOCOMOTION_SYSTEM_DYNAMICAL_SYSTEM_H
+#define BIPEDAL_LOCOMOTION_SYSTEM_DYNAMICAL_SYSTEM_H
+
+#include <memory>
+#include <tuple>
+
+#include <BipedalLocomotion/GenericContainer/TemplateHelpers.h>
+#include <BipedalLocomotion/ParametersHandler/IParametersHandler.h>
+
+namespace BipedalLocomotion
+{
+namespace System
+{
+
+// Declaration of a template
+template <typename StateTypeList, typename DerivativeTypeList, typename InputTypeList>
+class DynamicalSystem
+{
+    static_assert(dependent_false<StateTypeList>::value,
+                  "Unable to create the following dynamical system.");
+};
+
+/**
+ * DynamicalSystem defines a continuos time dynamical system, i.e. \f$\dot{x}=f(x, u, t)\f$. Please
+ * inherit publicly from this class  in order to define your custom dynamical system.
+ * @tparam StateTypes types used for describing the state (i.e. it may be a list of
+ * vectors/matrices).
+ * @tparam StateDerivativeTypes types used for describing the state derivative (i.e. it may be a
+ * list of vectors/matrices).
+ * @tparam InputTypes types used for describing the input (i.e. it may be a list
+ * of vectors/matrices or in general classes).
+ */
+template <typename... StateTypes, typename... StateDerivativeTypes, typename... InputTypes>
+class DynamicalSystem<std::tuple<StateTypes...>,
+                      std::tuple<StateDerivativeTypes...>,
+                      std::tuple<InputTypes...>>
+{
+protected:
+    std::tuple<StateTypes...> m_initialState;
+    std::tuple<InputTypes...> m_controlInput;
+
+public:
+    virtual bool initalize(std::weak_ptr<ParametersHandler::IParametersHandler> handler);
+
+    /**
+     * Set the initial state to the dynamical system.
+     * @notice In principle, there is no need to override this method. This value is stored in an
+     * internal buffer.
+     * @param initialState the initial state value.
+     * @return true in case of success, false otherwise.
+     */
+    virtual bool setInitialState(const std::tuple<StateTypes...>& initialState);
+
+    /**
+     * Set the control input to the dynamical system.
+     * @note In principle, there is no need to override this method. This value is stored in an
+     * internal buffer.
+     * @param controlInput the value of the control input used to compute the system dynamics.
+     * @return true in case of success, false otherwise.
+     */
+    virtual bool setControlInput(const std::tuple<InputTypes...>& controlInput);
+
+    /**
+     * Computes the system dynamics. It return \f$f(x, u, t)\f&.
+     * @note The control input has to be set separately with the method setControlInput.
+     * @param state tuple containing a const reference to the state elements.
+     * @param time the time at witch the dynamics is computed.
+     * @param stateDynamics tuple containing a reference to the element of the state derivative
+     * @return true in case of success, false otherwise.
+     */
+    virtual bool dynamics(const std::tuple<const StateTypes&...>& state,
+                          const double& time,
+                          const std::tuple<StateDerivativeTypes&...>& stateDerivative) = 0;
+
+    /**
+     * Destructor
+     */
+    ~DynamicalSystem() = default;
+};
+
+} // namespace System
+} // namespace BipedalLocomotion
+
+#include <BipedalLocomotion/System/DynamicalSystem.tpp>
+
+#endif // BIPEDAL_LOCOMOTION_SYSTEM_DYNAMICAL_SYSTEM_H
---
+++ src/System/include/BipedalLocomotion/System/DynamicalSystem.tpp
@@ -0,0 +1,50 @@
+/**
+ * @file DynamicalSystem.tpp
+ * @authors Giulio Romualdi
+ * @copyright 2020 Istituto Italiano di Tecnologia (IIT). This software may be modified and
+ * distributed under the terms of the GNU Lesser General Public License v2.1 or any later version.
+ */
+
+#ifndef BIPEDAL_LOCOMOTION_SYSTEM_DYNAMICAL_SYSTEM_TPP
+#define BIPEDAL_LOCOMOTION_SYSTEM_DYNAMICAL_SYSTEM_TPP
+
+#include <BipedalLocomotion/System/DynamicalSystem.h>
+
+namespace BipedalLocomotion
+{
+namespace System
+{
+
+template <typename... StateTypes, typename... StateDerivativeTypes, typename... InputTypes>
+bool DynamicalSystem<std::tuple<StateTypes...>,
+                     std::tuple<StateDerivativeTypes...>,
+                     std::tuple<InputTypes...>>::setInitialState(const std::tuple<StateTypes...>&
+                                                                     initialState)
+{
+    m_initialState = initialState;
+    return true;
+}
+
+template <typename... StateTypes, typename... StateDerivativeTypes, typename... InputTypes>
+bool DynamicalSystem<std::tuple<StateTypes...>,
+                     std::tuple<StateDerivativeTypes...>,
+                     std::tuple<InputTypes...>>::setControlInput(const std::tuple<InputTypes...>&
+                                                                     controlInput)
+{
+    m_controlInput = controlInput;
+    return true;
+}
+
+template <typename... StateTypes, typename... StateDerivativeTypes, typename... InputTypes>
+bool DynamicalSystem<std::tuple<StateTypes...>,
+                     std::tuple<StateDerivativeTypes...>,
+                     std::tuple<InputTypes...>>::
+    initalize(std::weak_ptr<ParametersHandler::IParametersHandler> handler)
+{
+    return true;
+}
+
+} // namespace System
+} // namespace BipedalLocomotion
+
+#endif // BIPEDAL_LOCOMOTION_SYSTEM_DYNAMICAL_SYSTEM_TPP
