f54eb54896ec99e458686bcc8c0b2f67a5ecf576
Giulio Romualdi <giulio.romualdi@gmail.com>
2020-05-22 12:57:43 +0200

    Improve the documentation for the LinearTimeInvariantSystem

--- src/System/include/BipedalLocomotion/System/LinearTimeInvariantSystem.h
+++ src/System/include/BipedalLocomotion/System/LinearTimeInvariantSystem.h
@@ -20,6 +20,19 @@ namespace BipedalLocomotion
 {
 namespace System
 {
+
+/**
+ * LinearTimeInvariantSystem describes a MIMO linear time invariant system of the form \f$\dot{x} =
+ * Ax + Bu\f$ where \a x is the state and \a u the control input. The state, its derivative and the
+ * control input are described by vectors
+ * The LinearTimeInvariantSystem inherits from a generic DynamicalSystem where:
+ * - DynamicalSystem::StateType is described by an std::tuple containing:
+ *   - iDynTree::VectorDynsize: a generic state.
+ * - DynamicalSystem::StateDerivativeType is described by an std::tuple containing:
+ *   - iDynTree::VectorDynsize: a generic state derivative.
+ * - DynamicalSystem::InputType is described by an std::tuple containing:
+ *   - iDynTree::VectorDynsize: a generic control input.
+ */
 class LinearTimeInvariantSystem : public DynamicalSystem<std::tuple<iDynTree::VectorDynSize>,
                                                          std::tuple<iDynTree::VectorDynSize>,
                                                          std::tuple<iDynTree::VectorDynSize>>
@@ -30,11 +43,30 @@ class LinearTimeInvariantSystem : public DynamicalSystem<std::tuple<iDynTree::Ve
     bool m_isInitialized{false};
 
 public:
+    /**
+     * Set the system matrices.
+     * @param A the A matrix.
+     * @param B the B matrix.
+     * @return true in case of success, false otherwise.
+     */
     bool setSystemMatrices(const iDynTree::MatrixDynSize& A, const iDynTree::MatrixDynSize& b);
 
+    /**
+     * Computes the linear system dynamics. It returns \f$\dot{x} = Ax + Bu\f$
+     * @note The control input has to be set separately with the method setControlInput.
+     * @param state tuple containing a const reference to the state elements.
+     * @param time the time at witch the dynamics is computed.
+     * @param stateDynamics tuple containing a reference to the element of the state derivative
+     * @return true in case of success, false otherwise.
+     */
     bool dynamics(const StateType& state,
                   const double& time,
                   StateDerivativeType& stateDerivative) final;
+
+    /**
+     * Destructor
+     */
+    ~LinearTimeInvariantSystem() = default;
 };
 
 } // namespace System
