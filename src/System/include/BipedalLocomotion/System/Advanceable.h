955f1193e1342d0bf84f76a6caff8cc027065fc5
Giulio Romualdi <giulio.romualdi@gmail.com>
2020-06-22 11:33:40 +0200

    Fix missing vitual keyword in Advanceable class

--- src/System/include/BipedalLocomotion/System/Advanceable.h
+++ src/System/include/BipedalLocomotion/System/Advanceable.h
@@ -30,19 +30,19 @@ public:
      * @brief Get the object.
      * @return a const reference of the requested object.
      */
-    const T& get() const = 0;
+    virtual const T& get() const = 0;
 
     /**
      * @brief Determines the validity of the object retrieved with get()
      * @return True if the object is valid, false otherwise.
      */
-    bool isValid() const = 0;
+    virtual bool isValid() const = 0;
 
     /**
      * @brief Advance the internal state. This may change the value retrievable from get().
      * @return True if the advance is successfull.
      */
-    bool advance() = 0;
+    virtual bool advance() = 0;
 };
 
 #endif //BIPEDAL_LOCOMOTION_SYSTEM_ADVANCEABLE_H
