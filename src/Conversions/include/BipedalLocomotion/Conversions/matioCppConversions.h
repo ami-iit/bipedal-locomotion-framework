312e61c65e0408a5d4836590643e4db50f2d63d4
Stefano <stefano.dafarra@gmail.com>
2020-11-13 20:02:12 +0100

    Added conversions from iDynTree types and added documentation.

--- src/Conversions/include/BipedalLocomotion/Conversions/matioCppConversions.h
+++ src/Conversions/include/BipedalLocomotion/Conversions/matioCppConversions.h
@@ -9,12 +9,20 @@
 #define BIPEDAL_LOCOMOTION_MATIOCPPCONVERSIONS_H
 
 #include <Eigen/Core>
+#include <iDynTree/Core/MatrixDynSize.h>
+#include <iDynTree/Core/EigenHelpers.h>
+#include <iDynTree/Core/MatrixFixSize.h>
 #include <matioCpp/matioCpp.h>
 #include <BipedalLocomotion/GenericContainer/Vector.h>
 
 namespace BipedalLocomotion {
 namespace Conversions {
 
+/**
+ * @brief Conversion from a matioCpp::MultiDimensionalArray to an Eigen matrix
+ * @param input The matioCpp::MultiDimensionalArray
+ * @return A map from the internal data of the MultiDimensionalArray
+ */
 template <typename type>
 Eigen::Map<Eigen::Matrix<type, Eigen::Dynamic, Eigen::Dynamic>> toEigen(matioCpp::MultiDimensionalArray<type>& input)
 {
@@ -23,6 +31,11 @@ Eigen::Map<Eigen::Matrix<type, Eigen::Dynamic, Eigen::Dynamic>> toEigen(matioCpp
     return Eigen::Map<Eigen::Matrix<type, Eigen::Dynamic, Eigen::Dynamic>>(input.data(), input.dimensions()(0), input.dimensions()(1));
 }
 
+/**
+ * @brief Conversion from a const matioCpp::MultiDimensionalArray to an Eigen matrix
+ * @param input The matioCpp::MultiDimensionalArray
+ * @return A const map from the internal data of the MultiDimensionalArray
+ */
 template <typename type>
 const Eigen::Map<Eigen::Matrix<type, Eigen::Dynamic, Eigen::Dynamic>> toEigen(const matioCpp::MultiDimensionalArray<type>& input)
 {
@@ -31,6 +44,11 @@ const Eigen::Map<Eigen::Matrix<type, Eigen::Dynamic, Eigen::Dynamic>> toEigen(co
     return Eigen::Map<const Eigen::Matrix<type, Eigen::Dynamic, Eigen::Dynamic>>(input.data(), input.dimensions()(0), input.dimensions()(1));
 }
 
+/**
+ * @brief Conversion from a matioCpp::Vector to an Eigen vector
+ * @param input The matioCpp::Vector
+ * @return A map from the internal data of the Vector
+ */
 template <typename type>
 Eigen::Map<Eigen::Matrix<type, Eigen::Dynamic, 1>> toEigen(matioCpp::Vector<type>& input)
 {
@@ -38,6 +56,11 @@ Eigen::Map<Eigen::Matrix<type, Eigen::Dynamic, 1>> toEigen(matioCpp::Vector<type
     return Eigen::Map<Eigen::Matrix<type, Eigen::Dynamic, 1>>(input.data(), input.size());
 }
 
+/**
+ * @brief Conversion from a const matioCpp::Vector to an Eigen vector
+ * @param input The matioCpp::Vector
+ * @return A const map from the internal data of the Vector
+ */
 template <typename type>
 const Eigen::Map<Eigen::Matrix<type, Eigen::Dynamic, 1>> toEigen(const matioCpp::Vector<type>& input)
 {
@@ -45,6 +68,12 @@ const Eigen::Map<Eigen::Matrix<type, Eigen::Dynamic, 1>> toEigen(const matioCpp:
     return Eigen::Map<const Eigen::Matrix<type, Eigen::Dynamic, 1>>(input.data(), input.size());
 }
 
+/**
+ * @brief Conversion from an Eigen matrix to a matioCpp::MultiDimensionalArray
+ * @param input The input matrix.
+ * @param name The name of the resulting matioCpp variable.
+ * @return A matioCpp::MultiDimensionalArray containing a copy of the input data
+ */
 template <typename EigenDerived, typename = std::enable_if_t<Eigen::MatrixBase<EigenDerived>::RowsAtCompileTime != 1 &&
                                                              Eigen::MatrixBase<EigenDerived>::ColsAtCompileTime != 1>>
 matioCpp::MultiDimensionalArray<typename EigenDerived::Scalar> tomatioCpp(const Eigen::MatrixBase<EigenDerived>& input, const std::string& name)
@@ -54,6 +83,37 @@ matioCpp::MultiDimensionalArray<typename EigenDerived::Scalar> tomatioCpp(const 
     return matio;
 }
 
+/**
+ * @brief Conversion from an iDynTree::MatrixDynSize to a matioCpp::MultiDimensionalArray
+ * @param input The input matrix.
+ * @param name The name of the resulting matioCpp variable.
+ * @return A matioCpp::MultiDimensionalArray containing a copy of the input data
+ */
+inline matioCpp::MultiDimensionalArray<double> tomatioCpp(const iDynTree::MatrixDynSize& input, const std::string& name)
+{
+    Eigen::MatrixXd eigenCopy = iDynTree::toEigen(input); //Need to make it column major first
+    return tomatioCpp(eigenCopy, name);
+}
+
+/**
+ * @brief Conversion from an iDynTree::MatrixFixSize to a matioCpp::MultiDimensionalArray
+ * @param input The input matrix.
+ * @param name The name of the resulting matioCpp variable.
+ * @return A matioCpp::MultiDimensionalArray containing a copy of the input data
+ */
+template<unsigned int nRows, unsigned int nCols>
+matioCpp::MultiDimensionalArray<double> tomatioCpp(const iDynTree::MatrixFixSize<nRows, nCols>& input, const std::string& name)
+{
+    Eigen::Matrix<double, nRows, nCols> eigenCopy = iDynTree::toEigen(input); //Need to make it column major first
+    return tomatioCpp(eigenCopy, name);
+}
+
+/**
+ * @brief Conversion from a generic Vector to a matioCpp::Vector
+ * @param input The input vector.
+ * @param name The name of the resulting matioCpp variable.
+ * @return A matioCpp::Vector containing a copy of the input data
+ */
 template <class Vector, typename = typename std::enable_if_t<BipedalLocomotion::GenericContainer::is_vector_constructible<Vector>::value &&
                                                              !std::is_same_v<Vector, std::string>>>
 matioCpp::Vector<typename std::remove_cv_t<typename BipedalLocomotion::container_data<Vector>::type>> tomatioCpp(const Vector& input, const std::string& name)
@@ -63,22 +123,46 @@ matioCpp::Vector<typename std::remove_cv_t<typename BipedalLocomotion::container
     return matioCpp::Vector<typename std::remove_cv_t<type>>(name, ref); //data is copied
 }
 
+/**
+ * @brief Conversion from a std::string to a matioCpp::String
+ * @param input The input string.
+ * @param name The name of the resulting matioCpp variable.
+ * @return A matioCpp::String containing a copy of the input data
+ */
 inline matioCpp::String tomatioCpp(const std::string& input, const std::string& name)
 {
     return matioCpp::String(name, input);
 }
 
+/**
+ * @brief Conversion from a boolean vector to a matioCpp::Vector<matioCpp::Logical>
+ * @param input The input vector.
+ * @param name The name of the resulting matioCpp variable.
+ * @return A matioCpp::Vector<matioCpp::Logical> containing a copy of the input data
+ */
 inline matioCpp::Vector<matioCpp::Logical> tomatioCpp(const std::vector<bool>& input, const std::string& name)
 {
     return matioCpp::Vector<matioCpp::Logical>(name, input);
 }
 
+/**
+ * @brief Conversion from a fundamental type to the corresponding matioCpp::Element
+ * @param input The input element.
+ * @param name The name of the resulting matioCpp variable.
+ * @return A matioCpp::Element containing a copy of the input data
+ */
 template<typename type, typename = std::enable_if_t<std::is_fundamental_v<type> && !std::is_same_v<type, bool>>>
 matioCpp::Element<type> tomatioCpp(type input, const std::string& name)
 {
     return matioCpp::Element<type>(name, input);
 }
 
+/**
+ * @brief Conversion from a boolean to a matioCpp::Element<matioCpp::Logical>
+ * @param input The input element.
+ * @param name The name of the resulting matioCpp variable.
+ * @return A matioCpp::Element<matioCpp::Logical> whose value is equal to the input.
+ */
 inline matioCpp::Element<matioCpp::Logical> tomatioCpp(bool input, const std::string& name)
 {
     return matioCpp::Element<matioCpp::Logical>(name, input);
--- src/Conversions/tests/matioCppConversionsTest.cpp
+++ src/Conversions/tests/matioCppConversionsTest.cpp
@@ -8,6 +8,7 @@
 // Catch2
 #include <catch2/catch.hpp>
 #include <BipedalLocomotion/Conversions/matioCppConversions.h>
+#include <iDynTree/Core/VectorDynSize.h>
 
 using namespace BipedalLocomotion::Conversions;
 
@@ -41,7 +42,7 @@ void checkSameMatrix(const Matrix& a, const matioCpp::MultiDimensionalArray<type
 
 TEST_CASE("From vector to matio")
 {
-    std::vector<double> stdVec = {1, 2, 3, 4, 5};
+    std::vector<double> stdVec = {1.0, 2.0, 3.0, 4.0, 5.0};
 
     auto toMatio = tomatioCpp(stdVec, "test");
 
@@ -57,6 +58,16 @@ TEST_CASE("From vector to matio")
     auto toMatioMatrix = tomatioCpp(eigenMatrix, "testMatrix");
     checkSameMatrix(eigenMatrix, toMatioMatrix);
 
+    iDynTree::MatrixDynSize iDynTreeMatrix(2,2);
+    iDynTree::toEigen(iDynTreeMatrix) << 1.0, 2.0, 3.0, 4.0;
+    auto toMatioMatrixiDynTree = tomatioCpp(iDynTreeMatrix, "iDynTreeMatrix");
+    checkSameMatrix(iDynTreeMatrix, toMatioMatrixiDynTree);
+
+    iDynTree::MatrixFixSize<2,2> iDynTreeMatrixFixed;
+    iDynTreeMatrixFixed = iDynTreeMatrix;
+    auto toMatioMatrixiDynTreeFixed = tomatioCpp(iDynTreeMatrixFixed, "iDynTreeMatrixFixed");
+    checkSameMatrix(iDynTreeMatrixFixed, toMatioMatrixiDynTreeFixed);
+
     std::string string("something");
     auto toMatioString = tomatioCpp(string, "name");
     REQUIRE(string == toMatioString());
@@ -64,4 +75,36 @@ TEST_CASE("From vector to matio")
     std::vector<bool> vecOfBool = {true, false, true};
     auto toVecofBool = tomatioCpp(vecOfBool, "vecOfBool");
     checkSameVectors(vecOfBool, toVecofBool);
+
+    auto matioDouble = tomatioCpp(5.0, "double");
+    REQUIRE(matioDouble == 5.0);
+
+    auto matioBool = tomatioCpp(true, "bool");
+    REQUIRE(matioBool);
+
+    auto matioInt = tomatioCpp(2, "bool");
+    REQUIRE(matioInt == 2);
+
+    auto matioChar = tomatioCpp('f', "char");
+    REQUIRE(matioChar == 'f');
+}
+
+TEST_CASE("From matioCpp to Eigen")
+{
+    std::vector<double> stdVec = {1.0, 2.0, 3.0, 4.0, 5.0};
+
+    matioCpp::Vector<double> vector("vector", 5);
+    vector = stdVec;
+
+    Eigen::VectorXd eigenVec = toEigen(vector);
+    checkSameVectors(eigenVec, vector);
+
+    Eigen::Matrix3f eigenMatrix;
+    eigenMatrix << 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0;
+
+    matioCpp::MultiDimensionalArray<float> matioCppMatrix("matrix", {3,3}, eigenMatrix.data());
+
+    Eigen::MatrixXf toEigenMatrix = toEigen(matioCppMatrix);
+    checkSameMatrix(toEigenMatrix, matioCppMatrix);
+
 }
