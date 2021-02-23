fb581921419422ec9cd4ddee986cbf6a8b2b1b67
Giulio Romualdi <giulio.romualdi@gmail.com>
2021-02-23 17:30:02 +0100

    Deprecate Contact class in Contact namespace

--- src/Planners/include/BipedalLocomotion/Planners/Contact.h
+++ src/Planners/include/BipedalLocomotion/Planners/Contact.h
@@ -8,7 +8,8 @@
 #ifndef BIPEDAL_LOCOMOTION_PLANNERS_CONTACT_H
 #define BIPEDAL_LOCOMOTION_PLANNERS_CONTACT_H
 
-#include <manif/SE3.h>
+#include <BipedalLocomotion/Contacts/Contact.h>
+
 
 #include <string>
 
@@ -17,57 +18,13 @@ namespace BipedalLocomotion
 namespace Planners
 {
 
-/**
- * @brief Definition of the type of contact
- */
-enum class ContactType
-{
-    /**
-    * The contact impose a full pose constraint.
-    */
-    FULL,
+[[deprecated("BipedalLocomotion::Planners::ContactType is deprecated; use "
+             "BipedalLocomotion::Contacts::ContactType instead.")]]
+typedef BipedalLocomotion::Contacts::ContactType ContactType;
 
-    /**
-    * The contact impose a position constraint.
-    */
-    POINT
-};
-
-struct Contact
-{
-    /**
-     * Pose of the contact.
-     */
-    manif::SE3d pose{manif::SE3d::Identity()};
-
-    /**
-     * Instant from which the contact can be considered active.
-     */
-    double activationTime {0.0};
-
-    /**
-     * Instant after which the contact is no more active.
-     */
-    double deactivationTime {0.0};
-
-    /**
-     * Name of the contact.
-     */
-    std::string name {"Contact"};
-
-    /**
-     * Type of contact.
-     */
-    ContactType type {ContactType::FULL};
-
-    /**
-     * @brief The equality operator.
-     *
-     * @param other The other object used for the comparison.
-     * @return True if the contacts are the same, false otherwise.
-     */
-    bool operator==(const Contact& other) const;
-};
+[[deprecated("BipedalLocomotion::Planners::Contact is deprecated; use "
+             "BipedalLocomotion::Contacts::PlannedContact instead.")]]
+typedef BipedalLocomotion::Contacts::PlannedContact Contact;
 
 }
 }
