4ec171dc2c66d7d38cf7956047401979eca1630b
Giulio Romualdi <giulio.romualdi@gmail.com>
2021-02-25 11:40:40 +0100

    Fix typo in Constants.h

--- src/Math/include/BipedalLocomotion/Math/Constants.h
+++ src/Math/include/BipedalLocomotion/Math/Constants.h
@@ -1,5 +1,5 @@
 /**
- * @file Costants.h
+ * @file Constants.h
  * @authors Giulio Romualdi
  * @copyright 2021 Istituto Italiano di Tecnologia (IIT). This software may be modified and
  * distributed under the terms of the GNU Lesser General Public License v2.1 or any later version.
