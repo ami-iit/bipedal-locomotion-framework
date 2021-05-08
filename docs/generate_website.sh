0f1442e4554526aed6f565f510698b22e0d9ac29
Giulio Romualdi <giulio.romualdi@gmail.com>
2021-05-08 12:10:58 +0200

    Fix bug in generate_website.sh

--- docs/generate_website.sh
+++ docs/generate_website.sh
@@ -18,11 +18,11 @@ do
     cd bipedal-locomotion-framework
     git checkout ${versions[i]}
     cd ..
-    python3 generate_documentation_files.py --tag ${versions[i]} --src_folder=bipedal-locomotion-framework --main_page=bipedal-locomotion-framework/${main_pages[i]}
+    python3 generate_documentation_files.py --tag ${versions[i]} --src_folder=bipedal-locomotion-framework/src --main_page=bipedal-locomotion-framework/${main_pages[i]}
 
     python3 ${mcss_script} conf-${versions[i]}.py
 
 done
 
-python3 generate_documentation_files.py --src_folder=../ --main_page=./pages/main-page.dox
+python3 generate_documentation_files.py --src_folder=../src --main_page=./pages/main-page.dox
 python3 ${mcss_script} conf-master.py
