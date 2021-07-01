e42d19aca7e97ccc5d8356977a59f4742d2b6923
Giulio Romualdi <giulio.romualdi@gmail.com>
2021-07-01 22:03:51 +0200

    Implement Contact wrench Cone unit test

--- src/Math/tests/FrictionConesTest.cpp
+++ src/Math/tests/FrictionConesTest.cpp
@@ -9,6 +9,7 @@
 #include <catch2/catch.hpp>
 
 #include <BipedalLocomotion/Math/LinearizedFrictionCone.h>
+#include <BipedalLocomotion/Math/ContactWrenchCone.h>
 #include <BipedalLocomotion/ParametersHandler/StdImplementation.h>
 
 using namespace BipedalLocomotion::Math;
@@ -37,4 +38,37 @@ TEST_CASE("Linearized Friction Cone")
 
     constexpr double tolerance = 1e-4;
     REQUIRE(matlabSolution.isApprox(cone.getA(), tolerance));
+}
+
+
+TEST_CASE("Contact wrench Cone")
+{
+    auto params = std::make_shared<StdImplementation>();
+    params->setParameter("number_of_slices", 2);
+    params->setParameter("static_friction_coefficient", 0.3);
+    params->setParameter("foot_limits_x", std::vector<double>{-0.05, 0.1});
+    params->setParameter("foot_limits_y", std::vector<double>{-0.05, 0.05});
+
+    ContactWrenchCone cone;
+    REQUIRE(cone.initialize(params));
+
+    // test the solution
+    REQUIRE(cone.getB().isApprox(Eigen::VectorXd::Zero(8 + 12)));
+
+    std::cerr << cone.getA() << std::endl;
+
+
+    Eigen::MatrixXd forceConstraintSolution(8, 3);
+    forceConstraintSolution << 2.4142,  1.0000, -0.7243,
+                               0.4142,  1.0000, -0.3000,
+                              -0.4142,  1.0000, -0.3000,
+                              -2.4142,  1.0000, -0.7243,
+                              -2.4142, -1.0000, -0.7243,
+                              -0.4142, -1.0000, -0.3000,
+                               0.4142, -1.0000, -0.3000,
+                               2.4142, -1.0000, -0.7243;
+
+    constexpr double tolerance = 1e-4;
+    REQUIRE(forceConstraintSolution.isApprox(cone.getA().topLeftCorner<8, 3>(), tolerance));
+    REQUIRE(cone.getA().topRightCorner<8, 3>().isZero(tolerance));
 }
