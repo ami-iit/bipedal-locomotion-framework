15dcd9c35e9c00ae058c7e89eaa5631577a45dd9
Giulio Romualdi <giulio.romualdi@gmail.com>
2021-07-02 14:08:50 +0200

    Improve the Contact wrench Cone test

--- src/Math/tests/FrictionConesTest.cpp
+++ src/Math/tests/FrictionConesTest.cpp
@@ -44,18 +44,18 @@ TEST_CASE("Linearized Friction Cone")
 TEST_CASE("Contact wrench Cone")
 {
     auto params = std::make_shared<StdImplementation>();
+    std::vector<double>limitsX{-0.05, 0.1};
+    std::vector<double>limitsY{-0.02, 0.01};
     params->setParameter("number_of_slices", 2);
     params->setParameter("static_friction_coefficient", 0.3);
-    params->setParameter("foot_limits_x", std::vector<double>{-0.05, 0.1});
-    params->setParameter("foot_limits_y", std::vector<double>{-0.05, 0.05});
+    params->setParameter("foot_limits_x", limitsX);
+    params->setParameter("foot_limits_y", limitsY);
 
     ContactWrenchCone cone;
     REQUIRE(cone.initialize(params));
 
     // test the solution
     REQUIRE(cone.getB().isApprox(Eigen::VectorXd::Zero(8 + 12)));
-
-    std::cerr << cone.getA() << std::endl;
 
 
     Eigen::MatrixXd forceConstraintSolution(8, 3);
@@ -71,4 +71,24 @@ TEST_CASE("Contact wrench Cone")
     constexpr double tolerance = 1e-4;
     REQUIRE(forceConstraintSolution.isApprox(cone.getA().topLeftCorner<8, 3>(), tolerance));
     REQUIRE(cone.getA().topRightCorner<8, 3>().isZero(tolerance));
+
+    Eigen::MatrixXd frictionConeConstraint(4, 6);
+    frictionConeConstraint << 0, 0,  limitsY[0], -1,  0, 0,
+                              0, 0, -limitsY[1],  1,  0, 0,
+                              0, 0,  limitsX[0],  0,  1, 0,
+                              0, 0, -limitsX[1],  0, -1, 0;
+
+    REQUIRE(cone.getA().middleRows(8, 4).isApprox(frictionConeConstraint, tolerance));
+
+    Eigen::MatrixXd yawTorqueConstriantMatlab(8,6);
+    yawTorqueConstriantMatlab << -0.01, -0.05, -0.018,  0.300,  0.300, -1.0000,
+                                 -0.01,  0.10, -0.033,  0.300, -0.300, -1.0000,
+                                  0.02, -0.05, -0.021, -0.300,  0.300, -1.0000,
+                                  0.02,  0.10, -0.036, -0.300, -0.300, -1.0000,
+                                  0.01,  0.05, -0.018,  0.300,  0.300,  1.0000,
+                                  0.01, -0.10, -0.033,  0.300, -0.300,  1.0000,
+                                 -0.02,  0.05, -0.021, -0.300,  0.300,  1.0000,
+                                 -0.02, -0.10, -0.036, -0.300, -0.300,  1.0000;
+
+    REQUIRE(cone.getA().bottomRows(8).isApprox(yawTorqueConstriantMatlab, tolerance));
 }
