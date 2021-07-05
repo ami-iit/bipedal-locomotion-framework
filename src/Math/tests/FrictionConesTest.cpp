03ad3e7b568da735aeda99bfe22ba0398a45c776
Giulio Romualdi <giulio.romualdi@gmail.com>
2021-07-05 14:42:08 +0200

    Improve Contact wrench Cone test

--- src/Math/tests/FrictionConesTest.cpp
+++ src/Math/tests/FrictionConesTest.cpp
@@ -8,8 +8,9 @@
 // Catch2
 #include <catch2/catch.hpp>
 
-#include <BipedalLocomotion/Math/LinearizedFrictionCone.h>
 #include <BipedalLocomotion/Math/ContactWrenchCone.h>
+#include <BipedalLocomotion/Math/LinearizedFrictionCone.h>
+#include <BipedalLocomotion/Math/Wrench.h>
 #include <BipedalLocomotion/ParametersHandler/StdImplementation.h>
 
 using namespace BipedalLocomotion::Math;
@@ -54,41 +55,94 @@ TEST_CASE("Contact wrench Cone")
     ContactWrenchCone cone;
     REQUIRE(cone.initialize(params));
 
-    // test the solution
-    REQUIRE(cone.getB().isApprox(Eigen::VectorXd::Zero(8 + 12)));
+    SECTION("Check matrix and vector")
+    {
+        // test the solution
+        REQUIRE(cone.getB().isApprox(Eigen::VectorXd::Zero(8 + 12)));
 
+        Eigen::MatrixXd forceConstraintSolution(8, 3);
+        forceConstraintSolution << 2.4142,  1.0000, -0.7243,
+                                   0.4142,  1.0000, -0.3000,
+                                  -0.4142,  1.0000, -0.3000,
+                                  -2.4142,  1.0000, -0.7243,
+                                  -2.4142, -1.0000, -0.7243,
+                                  -0.4142, -1.0000, -0.3000,
+                                   0.4142, -1.0000, -0.3000,
+                                   2.4142, -1.0000, -0.7243;
 
-    Eigen::MatrixXd forceConstraintSolution(8, 3);
-    forceConstraintSolution << 2.4142,  1.0000, -0.7243,
-                               0.4142,  1.0000, -0.3000,
-                              -0.4142,  1.0000, -0.3000,
-                              -2.4142,  1.0000, -0.7243,
-                              -2.4142, -1.0000, -0.7243,
-                              -0.4142, -1.0000, -0.3000,
-                               0.4142, -1.0000, -0.3000,
-                               2.4142, -1.0000, -0.7243;
+        constexpr double tolerance = 1e-4;
+        REQUIRE(forceConstraintSolution.isApprox(cone.getA().topLeftCorner<8, 3>(), tolerance));
+        REQUIRE(cone.getA().topRightCorner<8, 3>().isZero(tolerance));
 
-    constexpr double tolerance = 1e-4;
-    REQUIRE(forceConstraintSolution.isApprox(cone.getA().topLeftCorner<8, 3>(), tolerance));
-    REQUIRE(cone.getA().topRightCorner<8, 3>().isZero(tolerance));
+        Eigen::MatrixXd frictionConeConstraint(4, 6);
+        frictionConeConstraint << 0, 0,  limitsY[0], -1,  0, 0,
+                                  0, 0, -limitsY[1],  1,  0, 0,
+                                  0, 0,  limitsX[0],  0,  1, 0,
+                                  0, 0, -limitsX[1],  0, -1, 0;
 
-    Eigen::MatrixXd frictionConeConstraint(4, 6);
-    frictionConeConstraint << 0, 0,  limitsY[0], -1,  0, 0,
-                              0, 0, -limitsY[1],  1,  0, 0,
-                              0, 0,  limitsX[0],  0,  1, 0,
-                              0, 0, -limitsX[1],  0, -1, 0;
+        REQUIRE(cone.getA().middleRows(8, 4).isApprox(frictionConeConstraint, tolerance));
 
-    REQUIRE(cone.getA().middleRows(8, 4).isApprox(frictionConeConstraint, tolerance));
+        Eigen::MatrixXd yawTorqueConstriantMatlab(8,6);
+        yawTorqueConstriantMatlab << -0.01, -0.05, -0.018,  0.300,  0.300, -1.0000,
+                                     -0.01,  0.10, -0.033,  0.300, -0.300, -1.0000,
+                                      0.02, -0.05, -0.021, -0.300,  0.300, -1.0000,
+                                      0.02,  0.10, -0.036, -0.300, -0.300, -1.0000,
+                                      0.01,  0.05, -0.018,  0.300,  0.300,  1.0000,
+                                      0.01, -0.10, -0.033,  0.300, -0.300,  1.0000,
+                                     -0.02,  0.05, -0.021, -0.300,  0.300,  1.0000,
+                                     -0.02, -0.10, -0.036, -0.300, -0.300,  1.0000;
 
-    Eigen::MatrixXd yawTorqueConstriantMatlab(8,6);
-    yawTorqueConstriantMatlab << -0.01, -0.05, -0.018,  0.300,  0.300, -1.0000,
-                                 -0.01,  0.10, -0.033,  0.300, -0.300, -1.0000,
-                                  0.02, -0.05, -0.021, -0.300,  0.300, -1.0000,
-                                  0.02,  0.10, -0.036, -0.300, -0.300, -1.0000,
-                                  0.01,  0.05, -0.018,  0.300,  0.300,  1.0000,
-                                  0.01, -0.10, -0.033,  0.300, -0.300,  1.0000,
-                                 -0.02,  0.05, -0.021, -0.300,  0.300,  1.0000,
-                                 -0.02, -0.10, -0.036, -0.300, -0.300,  1.0000;
+        REQUIRE(cone.getA().bottomRows(8).isApprox(yawTorqueConstriantMatlab, tolerance));
+    }
 
-    REQUIRE(cone.getA().bottomRows(8).isApprox(yawTorqueConstriantMatlab, tolerance));
+    SECTION("Feasible wrench")
+    {
+        constexpr double forceZ = 100; // normal force in Newton
+
+        // the CoP is feasible since it is inside the rectangular foot.
+        Eigen::Vector2d CoP;
+        CoP << 0.01, -0.01;
+        constexpr double scalingTangentialForce = 0.01;
+
+        const double forceX = forceZ * scalingTangentialForce;
+        const double forceY = -forceZ * scalingTangentialForce;
+        const double torqueX = forceZ * CoP(1);
+        const double torqueY = -forceZ * CoP(0);
+        const double torqueZ = 0;
+
+        BipedalLocomotion::Math::Wrenchd wrench;
+        wrench << forceX, forceY, forceZ, torqueX, torqueY, torqueZ;
+
+        // Check feasibility
+        // Ax - b <= 0
+        Eigen::VectorXd constraintValue = -cone.getB();
+        constraintValue.noalias() += cone.getA() * wrench;
+        REQUIRE((constraintValue.array() < 0.0).all());
+    }
+
+    SECTION("Unfeasible wrench")
+    {
+        constexpr double forceZ = 100; // normal force in Newton
+
+        // The following CoP is outside the rectangular foot. So it is unfeasible.
+        Eigen::Vector2d CoP;
+        CoP << 0.2, -0.4;
+
+        // The following
+        constexpr double scalingTangentialForce = 0.4;
+        const double forceX = forceZ * scalingTangentialForce;
+        const double forceY = -forceZ * scalingTangentialForce;
+        const double torqueX = forceZ * CoP(1);
+        const double torqueY = -forceZ * CoP(0);
+        const double torqueZ = 0;
+
+        BipedalLocomotion::Math::Wrenchd wrench;
+        wrench << forceX, forceY, forceZ, torqueX, torqueY, torqueZ;
+
+        // Check unfeasibility
+        // Ax - b > 0
+        Eigen::VectorXd constraintValue = -cone.getB();
+        constraintValue.noalias() += cone.getA() * wrench;
+        REQUIRE((constraintValue.array() > 0.0).any());
+    }
 }
