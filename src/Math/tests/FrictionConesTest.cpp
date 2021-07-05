1ea4a0a590f7d0019130d656c9708573698d8a1c
Giulio Romualdi <giulio.romualdi@gmail.com>
2021-07-05 12:05:37 +0200

    Fix typo in FrictionConesTest.cpp
    Co-authored-by: Prashanth Ramadoss <prashanthramadoss@gmail.com>

--- src/Math/tests/FrictionConesTest.cpp
+++ src/Math/tests/FrictionConesTest.cpp
@@ -1,5 +1,5 @@
 /**
- * @file LinearizedFrictionConeTest.cpp
+ * @file FrictionConesTest.cpp
  * @authors Giulio Romualdi
  * @copyright 2021 Istituto Italiano di Tecnologia (IIT). This software may be modified and
  * distributed under the terms of the GNU Lesser General Public License v2.1 or any later version.
