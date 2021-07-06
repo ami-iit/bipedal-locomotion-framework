6c177f260c5d5c7577eb6e9acb949b061fad5b53
Giulio Romualdi <giulio.romualdi@gmail.com>
2021-07-06 15:30:35 +0200

    Implement the test for the IK::SE3Task mask feature

--- src/IK/tests/SE3TaskTest.cpp
+++ src/IK/tests/SE3TaskTest.cpp
@@ -41,48 +41,49 @@ TEST_CASE("SE3 Task")
 
     for (std::size_t numberOfJoints = 6; numberOfJoints < 200; numberOfJoints += 15)
     {
-        DYNAMIC_SECTION("Model with " << numberOfJoints << " joints")
+        // create the model
+        const iDynTree::Model model = iDynTree::getRandomModel(numberOfJoints);
+        REQUIRE(kinDyn->loadRobotModel(model));
+
+        const auto worldBasePos = iDynTree::getRandomTransform();
+        const auto baseVel = iDynTree::getRandomTwist();
+        iDynTree::VectorDynSize jointsPos(model.getNrOfDOFs());
+        iDynTree::VectorDynSize jointsVel(model.getNrOfDOFs());
+        iDynTree::Vector3 gravity;
+
+        for (auto& joint : jointsPos)
         {
-            // create the model
-            const iDynTree::Model model = iDynTree::getRandomModel(numberOfJoints);
-            REQUIRE(kinDyn->loadRobotModel(model));
+            joint = iDynTree::getRandomDouble();
+        }
 
-            const auto worldBasePos = iDynTree::getRandomTransform();
-            const auto baseVel = iDynTree::getRandomTwist();
-            iDynTree::VectorDynSize jointsPos(model.getNrOfDOFs());
-            iDynTree::VectorDynSize jointsVel(model.getNrOfDOFs());
-            iDynTree::Vector3 gravity;
+        for (auto& joint : jointsVel)
+        {
+            joint = iDynTree::getRandomDouble();
+        }
 
-            for (auto& joint : jointsPos)
-            {
-                joint = iDynTree::getRandomDouble();
-            }
+        for (auto& element : gravity)
+        {
+            element = iDynTree::getRandomDouble();
+        }
 
-            for (auto& joint : jointsVel)
-            {
-                joint = iDynTree::getRandomDouble();
-            }
+        REQUIRE(kinDyn->setRobotState(worldBasePos, jointsPos, baseVel, jointsVel, gravity));
 
-            for (auto& element : gravity)
-            {
-                element = iDynTree::getRandomDouble();
-            }
+        // Instantiate the handler
+        VariablesHandler variablesHandler;
+        variablesHandler.addVariable("dummy1", 10);
+        variablesHandler.addVariable(robotVelocity, model.getNrOfDOFs() + 6);
+        variablesHandler.addVariable("dummy2", 15);
 
-            REQUIRE(kinDyn->setRobotState(worldBasePos, jointsPos, baseVel, jointsVel, gravity));
+        const std::string controlledFrame = model.getFrameName(numberOfJoints);
+        parameterHandler->setParameter("frame_name", controlledFrame);
 
-            // Instantiate the handler
-            VariablesHandler variablesHandler;
-            variablesHandler.addVariable("dummy1", 10);
-            variablesHandler.addVariable(robotVelocity, model.getNrOfDOFs() + 6);
-            variablesHandler.addVariable("dummy2", 15);
-
-            const std::string controlledFrame = model.getFrameName(numberOfJoints);
-            parameterHandler->setParameter("frame_name", controlledFrame);
-
+        DYNAMIC_SECTION("Model with " << numberOfJoints << " joints - [All DoF]")
+        {
             SE3Task task;
             REQUIRE(task.setKinDyn(kinDyn));
             REQUIRE(task.initialize(parameterHandler));
             REQUIRE(task.setVariablesHandler(variablesHandler));
+            REQUIRE(task.size() == 6);
 
             const auto desiredPose = manif::SE3d::Random();
             const auto desiredVelocity = manif::SE3d::Tangent::Random();
@@ -99,18 +100,18 @@ TEST_CASE("SE3 Task")
             // check the matrix A
             REQUIRE(A.middleCols(variablesHandler.getVariable("dummy1").offset,
                                  variablesHandler.getVariable("dummy1").size)
-                        .isZero());
+                    .isZero());
 
             REQUIRE(A.middleCols(variablesHandler.getVariable("dummy2").offset,
                                  variablesHandler.getVariable("dummy2").size)
-                        .isZero());
+                    .isZero());
 
             Eigen::MatrixXd jacobian(6, model.getNrOfDOFs() + 6);
             REQUIRE(kinDyn->getFrameFreeFloatingJacobian(controlledFrame, jacobian));
 
             REQUIRE(A.middleCols(variablesHandler.getVariable(robotVelocity).offset,
                                  variablesHandler.getVariable(robotVelocity).size)
-                        .isApprox(jacobian));
+                    .isApprox(jacobian));
 
             // check the vector b
             LieGroupControllers::ProportionalControllerSO3d SO3Controller;
@@ -124,7 +125,7 @@ TEST_CASE("SE3 Task")
             R3Controller.setDesiredState(desiredPose.translation());
 
             SO3Controller.setState(BipedalLocomotion::Conversions::toManifRot(
-                kinDyn->getWorldTransform(controlledFrame).getRotation()));
+                                       kinDyn->getWorldTransform(controlledFrame).getRotation()));
 
             R3Controller.setState(
                 iDynTree::toEigen(kinDyn->getWorldTransform(controlledFrame).getPosition()));
@@ -135,6 +136,103 @@ TEST_CASE("SE3 Task")
             Eigen::VectorXd expectedB(6);
             expectedB.head<3>() = R3Controller.getControl().coeffs();
             expectedB.tail<3>() = SO3Controller.getControl().coeffs();
+
+            REQUIRE(b.isApprox(expectedB));
+        }
+
+
+        DYNAMIC_SECTION("Model with " << numberOfJoints << " joints - [mask]")
+        {
+            const auto mask = std::vector<bool>{true, false, true, false, false, false};
+            const auto DoFs = std::count(mask.begin(), mask.end(), true);
+
+            parameterHandler->setParameter("mask", mask);
+
+            SE3Task task;
+            REQUIRE(task.setKinDyn(kinDyn));
+            REQUIRE(task.initialize(parameterHandler));
+            REQUIRE(task.setVariablesHandler(variablesHandler));
+            REQUIRE(task.size() == DoFs);
+
+            const auto desiredPose = manif::SE3d::Random();
+            const auto desiredVelocity = manif::SE3d::Tangent::Random();
+
+            REQUIRE(task.setSetPoint(desiredPose, desiredVelocity));
+
+            REQUIRE(task.update());
+            REQUIRE(task.isValid());
+
+            // get A and b
+            Eigen::Ref<const Eigen::MatrixXd> A = task.getA();
+            Eigen::Ref<const Eigen::VectorXd> b = task.getB();
+
+            // check the matrix A
+            REQUIRE(A.middleCols(variablesHandler.getVariable("dummy1").offset,
+                                 variablesHandler.getVariable("dummy1").size)
+                    .isZero());
+
+            REQUIRE(A.middleCols(variablesHandler.getVariable("dummy2").offset,
+                                 variablesHandler.getVariable("dummy2").size)
+                    .isZero());
+
+            Eigen::MatrixXd jacobian(6, model.getNrOfDOFs() + 6);
+            REQUIRE(kinDyn->getFrameFreeFloatingJacobian(controlledFrame, jacobian));
+
+            // extract the
+            Eigen::MatrixXd jacobianWithMask(DoFs, model.getNrOfDOFs() + 6);
+            std::size_t index = 0;
+            for (std::size_t i = 0; i < 6; i++)
+            {
+                if (mask[i])
+                {
+                    jacobianWithMask.row(index) = jacobian.row(i);
+                    index++;
+                }
+            }
+
+            REQUIRE(A.middleCols(variablesHandler.getVariable(robotVelocity).offset,
+                                 variablesHandler.getVariable(robotVelocity).size)
+                    .isApprox(jacobianWithMask));
+
+            // check the vector b
+            LieGroupControllers::ProportionalControllerSO3d SO3Controller;
+            LieGroupControllers::ProportionalControllerR3d R3Controller;
+            SO3Controller.setGains(kp);
+            R3Controller.setGains(kp);
+
+            SO3Controller.setFeedForward(desiredVelocity.ang());
+            R3Controller.setFeedForward(desiredVelocity.lin());
+            SO3Controller.setDesiredState(desiredPose.quat());
+            R3Controller.setDesiredState(desiredPose.translation());
+
+            SO3Controller.setState(BipedalLocomotion::Conversions::toManifRot(
+                                       kinDyn->getWorldTransform(controlledFrame).getRotation()));
+
+            R3Controller.setState(
+                iDynTree::toEigen(kinDyn->getWorldTransform(controlledFrame).getPosition()));
+
+            SO3Controller.computeControlLaw();
+            R3Controller.computeControlLaw();
+
+            Eigen::VectorXd expectedB(DoFs);
+            index = 0;
+            for(int i = 0; i < 3; i++)
+            {
+                if(mask[i])
+                {
+                    expectedB(index) = R3Controller.getControl().coeffs()[i];
+                    index++;
+                }
+            }
+
+            for(int i = 0; i < 3; i++)
+            {
+                if(mask[i + 3])
+                {
+                    expectedB(index) = SO3Controller.getControl().coeffs()[i];
+                    index++;
+                }
+            }
 
             REQUIRE(b.isApprox(expectedB));
         }
