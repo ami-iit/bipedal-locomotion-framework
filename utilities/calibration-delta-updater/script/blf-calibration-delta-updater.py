429b531889d66106a5c8e63c10ba1dbff582ed67
Giulio Romualdi <giulio.romualdi@gmail.com>
2021-07-07 19:27:28 +0200

    Add the possibility to set an expected value in blf-calibration-delta-updater.py script

--- utilities/calibration-delta-updater/script/blf-calibration-delta-updater.py
+++ utilities/calibration-delta-updater/script/blf-calibration-delta-updater.py
@@ -9,21 +9,16 @@ from lxml import etree
 import re
 import math
 import numpy as np
+import os
 
 import bipedal_locomotion_framework.bindings as blf
+
 
 def print_info(msg):
     print('\33[104m' + '[offset_updater]' + '\033[0m ' + msg)
 
-def open_polydriver(robot_name, control_board, joints_list):
-    param_handler = blf.parameters_handler.StdParametersHandler()
-    param_handler.set_parameter_vector_string('joints_list', joints_list)
-    param_handler.set_parameter_vector_string('remote_control_boards', [control_board])
-    param_handler.set_parameter_string('robot_name', robot_name)
-    param_handler.set_parameter_string('local_prefix', 'joint_offset_updater')
-    param_handler.set_parameter_bool("check_for_nan", False)
-    param_handler.set_parameter_bool("stream_joint_states", True)
 
+def open_polydriver(param_handler):
     board = blf.robot_interface.construct_remote_control_board_remapper(param_handler)
 
     bridge = blf.robot_interface.YarpSensorBridge()
@@ -43,11 +38,13 @@ def get_offsets(xml_root):
 
     return deltas
 
+
 def prettify(elem):
     return etree.tostring(elem, encoding="UTF-8",
                    xml_declaration=True,
                    pretty_print=True,
                    doctype='<!DOCTYPE devices PUBLIC "-//YARP//DTD yarprobotinterface 3.0//EN" "http://www.yarp.it/DTD/yarprobotinterfaceV3.0.dtd">')
+
 
 def set_offsets(xml_root, offsets):
 
@@ -59,34 +56,66 @@ def set_offsets(xml_root, offsets):
 
     return xml_root
 
+
+def compute_joint_offset(joint_index, expected_values):
+    # read the joint state
+    assert sensor_bridge.advance() is True
+    _, joints_values, _ = sensor_bridge.get_joint_positions()
+    delta = joints_values[joint_index] * 180 / math.pi - expected_values[joint_index]
+    return delta
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Automatically update the calibration delta of the robot configuration file.')
     parser.add_argument('-i', '--input', type=str, required=True, help='Input xml file')
     parser.add_argument('-o', '--output', type=str, required=True, help='Output xml file')
-    parser.add_argument('-b', '--board', type=str, required=True, help='Name of the control board')
-    parser.add_argument('-j', '--joints', type=str, nargs='+', required=True, help='Name of the control board. The order must be the same of the one considered in the input xml')
-    parser.add_argument('-r', '--robot', type=str, required=True, help='Name of the robot')
+    parser.add_argument('--from', type=str, required=False, help='Path or name of the configuration file', default="blf-calibration-delta-update-options.ini")
+    parser.add_argument('-p', '--part', type=str, required=True, help='name of the part.')
 
     args = parser.parse_args()
 
+    # get the original offset
     tree = etree.parse(args.input)
     root = tree.getroot()
     offsets = get_offsets(root)
 
-    control_board, sensor_bridge = open_polydriver(args.robot, args.board, args.joints)
+    # load the parameter hander
+    parameterHandler = blf.parameters_handler.YarpParametersHandler()
 
-    print_info("Please put the joints in zero configuration.")
-    print_info("Press enter and I will show you the offsets.")
-    input()
+    if (os.path.isfile(args.from)):
+        assert parameterHandler.set_from_file_path(args.from) is True
+    else:
+        assert parameterHandler.set_from_filename(args.from) is True
 
-    assert sensor_bridge.advance() is True
+    group = parameterHandler.get_group(parser.part)
+    assert group is not None
+    joints_list = group.get_parameter_vector_string('joints_list')
+    control_board = group.get_parameter_string('control_board')
+    expected_values = param_handler.get_parameter_vector_double('expected_value')
+    param_handler.set_parameter_vector_string('joints_list', joints_list)
+    param_handler.set_parameter_vector_string('remote_control_boards', [control_board])
 
-    _, joints_values, _ = sensor_bridge.get_joint_positions()
-    new_offsets = np.array(offsets) + joints_values * 180 / math.pi
+    # Open the sensorbridge
+    control_board, sensor_bridge = open_polydriver(parameter_handler)
 
-    print_info("Joint values = " + str(['%.4f' % joint for joint in joints_values * 180 / math.pi]) + " deg")
+    new_offsets = []
+
+    for i, joint in enumerate(joints_list):
+        key = ""
+        while key != 'y' and key != 'n':
+            print_info("Do you want to calibrate the joint named: " + joint + " [y|n]")
+            key = input()
+            if key = 'n':
+                new_offsets.append(0);
+            elif key == 'y' :
+                print_info("Please move the joint in the expected configuration. Press enter when you are ready.")
+                input()
+                new_offsets.append(compute_joint_offset(i, expected_values))
+
+    updated_offset = np.array(offsets) + np.array(new_offsets)
     print_info("previous offset = " + str(['%.4f' % offset for offset in offsets]) + " deg")
-    print_info("new offset = " + str(['%.4f' % offset for offset in new_offsets]) + " deg")
+    print_info("new offset = " + str(['%.4f' % offset for offset in updated_offset]) + " deg")
+
 
     key = ""
     while key != 'y' and key != 'n':
@@ -95,7 +124,7 @@ if __name__ == '__main__':
         if key == 'n' :
             print_info("Offset not changed")
         elif key == 'y' :
-            root = set_offsets(root, new_offsets)
+            root = set_offsets(root, updated_offset)
 
             with open(args.output, 'bw') as f:
                 f.write(prettify(root))
