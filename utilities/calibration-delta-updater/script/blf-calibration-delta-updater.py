e82851299724fa2d9384efce5657194d89058a86
Giulio Romualdi <giulio.romualdi@gmail.com>
2021-07-09 12:56:52 +0200

    Take into account the tolerance blf-calibration-delta-updater

--- utilities/calibration-delta-updater/script/blf-calibration-delta-updater.py
+++ utilities/calibration-delta-updater/script/blf-calibration-delta-updater.py
@@ -61,23 +61,30 @@ def set_offsets(xml_root, offsets):
     return xml_root
 
 
-def compute_joint_offset(sensor_bridge, joint_index, expected_values):
+def compute_joint_offset(sensor_bridge, joint_index, expected_values, tolerances):
     # read the joint state
     if not sensor_bridge.advance():
-        raise RuntimeError('Unable to reda the sensors')
+        raise RuntimeError('Unable to read the sensors')
 
     _, joints_values, _ = sensor_bridge.get_joint_positions()
 
     # compute the offset
     delta = joints_values[joint_index] * 180 / math.pi - expected_values[joint_index]
-    return delta
+
+    tolerance = tolerances[joint_index]
+    if abs(delta) < tolerances[joint_index]:
+        print_info("The offset is equal to " + str(delta)
+                   + " deg. The absolute value is lower than the desired tolerance "
+                   + str(tolerance) + " deg.")
+        print_info("The offset is set to 0 deg")
+        return 0.0
+    else:
+        return delta
 
 
 def parse_handler(file, robot_part):
     # load the parameter handler
     parameters = blf.parameters_handler.YarpParametersHandler()
-
-    print("---------------------------------->" + file)
 
     if os.path.isfile(file):
         if not parameters.set_from_file_path(file):
@@ -92,27 +99,43 @@ def parse_handler(file, robot_part):
 
     joints = group.get_parameter_vector_string('joints_list')
     board = group.get_parameter_string('control_board')
+    values = group.get_parameter_vector_float('expected_values_in_degrees')
+    tolerance = group.get_parameter_vector_float('tolerance_in_degrees')
+
     parameters.set_parameter_vector_string('joints_list', joints)
     parameters.set_parameter_vector_string('remote_control_boards', [board])
-    values = group.get_parameter_vector_float('expected_values')
 
     # populate the parameters handler with additional parameters required by the application. There is no need to
     # expose these parameter in the configuration file
     parameters.set_parameter_string('local_prefix', 'joint-offset-updater')
     parameters.set_parameter_bool('check_for_nan', False)
     parameters.set_parameter_bool('stream_joint_states', True)
-    return parameters, values, joints
+    return parameters, values, joints, tolerance
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Automatically update the calibration delta of the robot '
-                                                 'configuration file.')
-    parser.add_argument('-i', '--input', type=str, required=True, help='Input xml file')
-    parser.add_argument('-o', '--output', type=str, required=True, help='Output xml file')
-    parser.add_argument('-p', '--part', type=str, required=True, help='Name of the part.')
-    parser.add_argument('--config', type=str, required=False, help='Path or name of the configuration file',
+    parser = argparse.ArgumentParser(description='Simple tool to semi-automatically update the calibration delta of a YARP-based robot.')
+    parser.add_argument('-i', '--input', type=str, required=True, help='Path to the input xml file containing the calibration deltas')
+    parser.add_argument('-o', '--output', type=str,
+                        required=True, help='Path to the output xml file containing the calibration deltas')
+    parser.add_argument('-p', '--part', type=str, required=True,
+                        help='Name of the group will be loaded in the configuration file. For instance  left_leg or right_leg.')
+    parser.add_argument('--config', type=str, required=False,
+                        help='Path to the configuration file loaded by the application. '
+                        'By default the blf-calibration-delta-updater uses YARP ResourceFinder '
+                        'to locate a file named blf-calibration-delta-updater-options.ini',
                         default="blf-calibration-delta-updater-options.ini")
     args = parser.parse_args()
+
+    # check if the input file exists
+    if not os.path.isfile(args.input):
+        raise RuntimeError('The file path ' + args.input + ' does not exist')
+
+    output_dir = os.path.dirname(args.output)
+
+    if output_dir and not os.path.isdir(output_dir):
+        raise RuntimeError('The directory named ' + os.path.dirname(args.output)  + ' does not exist')
+
 
     # get the original offset
     tree = etree.parse(args.input)
@@ -120,7 +143,7 @@ def main():
     offsets = get_offsets(root)
 
     # load the parameter handler
-    handler, expected_values, joints_list = parse_handler(args.config, args.part)
+    handler, expected_values, joints_list, tolerances = parse_handler(args.config, args.part)
 
     # Open the sensorbridge
     control_board, sensor_bridge = open_polydriver(handler)
@@ -136,8 +159,7 @@ def main():
             elif key == 'y':
                 print_info("Please move the joint in the expected configuration. Press enter when you are ready.")
                 input()
-                print(expected_values)
-                new_offsets.append(compute_joint_offset(sensor_bridge, i, expected_values))
+                new_offsets.append(compute_joint_offset(sensor_bridge, i, expected_values, tolerances))
 
     updated_offset = np.array(offsets) + np.array(new_offsets)
     print_info("Previous offsets = " + str(['%.4f' % offset for offset in offsets]) + " deg")
