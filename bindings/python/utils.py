64870db08f2bf2274c5169abf05a74de060335c1
Giulio Romualdi <giulio.romualdi@gmail.com>
2021-11-08 20:35:29 +0100

    Implement create_ik python function

--- bindings/python/utils.py
+++ bindings/python/utils.py
@@ -107,3 +107,87 @@ def create_tsid(kindyn: blf.floating_base_estimators.KinDynComputationsDescripto
     assert solver.finalize(variables_handler)
 
     return solver, tasks, variables_handler
+
+
+def create_ik(kindyn: blf.floating_base_estimators.KinDynComputationsDescriptor,
+              param_handler: blf.parameters_handler.IParametersHandler):
+    """create_ik is a function that will help you to create a IK problem. It creates a IK object with all the
+    associated tasks. The function returns also the variable handler associated
+
+    Parameters
+    ----------
+    kindyn : bipedal_locomotion_framework.floating_base_estimators.KinDynComputationsDescriptor
+       a `kindyn` computation object. The object will be shared among all the tasks
+    param_handler : bipedal_locomotion_framework.parameters_handler.IParametersHandler
+       The handler containing all the required parameter.
+       The following configuration file can be used as example
+
+       tasks = ["COM_TASK", "LF_TASK"]
+
+       [VARIABLES]
+       variables_name = ["robot_velocity"]
+       variables_size = [29]
+
+       [COM_TASK]
+       name = "com"
+       type = "CoMTask"
+       priority = 1
+       weight = [10.0, 10.0, 10.0]
+
+       # The following parameters are required by the specific task
+       robot_velocity_variable_name = "robot_velocity"
+       kp_linear = 10.0
+
+       [LF_TASK]
+       name = "left_foot"
+       type = "SE3Task"
+       priority = 0
+
+       # The following parameters are required by the specific task
+       robot_velocity_variable_name = "robot_velocity"
+       frame_name = "left_sole_link"
+       kp_linear = 10.0
+       kp_angular = 10.0
+
+    Returns
+    -------
+    tuple
+        a tuple containing the solver, a dictionary of the tasks and the variables handler
+    """
+
+    solver = blf.ik.QPInverseKinematics()
+    tasks = dict()
+    variables_handler = blf.system.VariablesHandler()
+
+    # initialize the variable handler
+    assert variables_handler.initialize(param_handler.get_group("VARIABLES"))
+
+    # retrieve all the tasks
+    task_groups_name = param_handler.get_parameter_vector_string("tasks")
+    for task_group_name in task_groups_name:
+        task_group = param_handler.get_group(task_group_name)
+        name = task_group.get_parameter_string("name")
+        if name in tasks.keys():
+            raise AssertionError("The task named ", name, " is not unique.")
+
+        # instantiate and initialize the the task
+        tasks[name] = getattr(blf.ik, task_group.get_parameter_string("type"))()
+
+        # a task may not have set_kin_dyn method
+        if hasattr(tasks[name], "set_kin_dyn") and callable(getattr(tasks[name], "set_kin_dyn")):
+            assert tasks[name].set_kin_dyn(kindyn.kindyn)
+
+        assert tasks[name].initialize(task_group)
+
+        # add the task to the optimization problem
+        priority = task_group.get_parameter_int("priority")
+        assert priority == 1 or priority == 0
+        if priority == 1:
+            assert solver.add_task(tasks[name], name, priority, task_group.get_parameter_vector_float("weight"))
+        else:
+            assert solver.add_task(tasks[name], name, priority)
+
+    # All the tasks have been added. It's time to finalize the problem
+    assert solver.finalize(variables_handler)
+
+    return solver, tasks, variables_handler
