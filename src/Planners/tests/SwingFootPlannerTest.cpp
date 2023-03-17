d467f4548f26975c747db6d57b9192122fa72379
Giulio Romualdi <giulio.romualdi@gmail.com>
2023-03-17 19:07:31 +0100

    Update the Planners tests according to the modification

--- src/Planners/tests/SwingFootPlannerTest.cpp
+++ src/Planners/tests/SwingFootPlannerTest.cpp
@@ -5,6 +5,8 @@
  * distributed under the terms of the BSD-3-Clause license.
  */
 
+#include <chrono>
+
 // Catch2
 #include <catch2/catch.hpp>
 
@@ -20,35 +22,37 @@ using namespace BipedalLocomotion::ParametersHandler;
 
 TEST_CASE("Swing foot planner")
 {
+    using namespace std::chrono_literals;
+
     ContactList contactList;
 
     // first footstep
     manif::SE3d transform{{0.9, 0, 0}, Eigen::AngleAxisd(1.5708, Eigen::Vector3d::UnitZ())};
-    REQUIRE(contactList.addContact(transform, 0.0, 0.3));
+    REQUIRE(contactList.addContact(transform, 0s, 300ms));
 
     // second
     transform = manif::SE3d({0.8899, 0.1345, 0.0600},
                             Eigen::AngleAxisd(1.7208, Eigen::Vector3d::UnitZ()));
-    REQUIRE(contactList.addContact(transform, 0.6, 1.5));
+    REQUIRE(contactList.addContact(transform, 600ms, 1s + 500ms));
 
     // third
     transform = manif::SE3d({0.8104, 0.3915, 0.1800},
                             Eigen::AngleAxisd(2.0208, Eigen::Vector3d::UnitZ()));
-    REQUIRE(contactList.addContact(transform, 1.8, 2.7));
+    REQUIRE(contactList.addContact(transform, 1s + 800ms, 2s + 700ms));
 
     // forth
     transform = manif::SE3d({0.6585, 0.6135, 0.3000},
                             Eigen::AngleAxisd(2.3208, Eigen::Vector3d::UnitZ()));
-    REQUIRE(contactList.addContact(transform, 3.0, 3.9));
+    REQUIRE(contactList.addContact(transform, 3s, 3s + 900ms));
 
     // fifth
     transform = manif::SE3d({0.3261, 0.8388, 0.4800},
                             Eigen::AngleAxisd(2.7708, Eigen::Vector3d::UnitZ()));
-    REQUIRE(contactList.addContact(transform, 4.2, 6.0));
+    REQUIRE(contactList.addContact(transform, 4s, 6s));
 
     // Set the parameters
     std::shared_ptr<IParametersHandler> handler = std::make_shared<StdImplementation>();
-    constexpr double dT = 0.01;
+    constexpr std::chrono::nanoseconds dT = 10ms;
     handler->setParameter("sampling_time", dT);
     handler->setParameter("step_height", 0.1);
     handler->setParameter("foot_apex_time", 0.5);
@@ -57,15 +61,15 @@ TEST_CASE("Swing foot planner")
     handler->setParameter("foot_take_off_velocity", 0.0);
     handler->setParameter("foot_take_off_acceleration", 0.0);
 
-
     // initialize the planner
     SwingFootPlanner planner;
     REQUIRE(planner.initialize(handler));
     planner.setContactList(contactList);
 
     std::cout << "pos = [";
-    const std::size_t numberOfIterations = (contactList.lastContact()->deactivationTime + 1) / dT;
-    for (std::size_t i = 0; i < numberOfIterations; i++)
+    for (std::chrono::nanoseconds time = contactList.firstContact()->activationTime;
+         time < contactList.lastContact()->deactivationTime;
+         time += dT)
     {
         std::cout << planner.getOutput().transform.translation().transpose() << " "
                   << planner.getOutput().transform.quat().w() << " "
