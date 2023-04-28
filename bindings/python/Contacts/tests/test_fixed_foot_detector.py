ami-iit/bipedal-locomotion-framework

commit 95a29b4089a34aa294a2107f867ac1db20b78059

Update ContactDetectors after the restructure of Contacts

src/Contacts/include/BipedalLocomotion/ContactDetectors/FixedFootDetector.h
@@ -13,6 +13,7 @@
 #include <BipedalLocomotion/Contacts/Contact.h>
 #include <BipedalLocomotion/ParametersHandler/IParametersHandler.h>
 
+#include <chrono>
 #include <memory>
 
 namespace BipedalLocomotion
@@ -67,8 +68,10 @@ namespace Contacts
 class FixedFootDetector : public ContactDetector
 {
     ContactPhaseList m_contactPhaselist; /**< List of the contacts */
-    double m_currentTime{0}; /**< Current time in seconds */
-    double m_dT{0}; /**< Fixed sampling time in seconds */
+    std::chrono::nanoseconds m_currentTime{std::chrono::nanoseconds::zero()}; /**< Current time in
+                                                                                 seconds */
+    std::chrono::nanoseconds m_dT{std::chrono::nanoseconds::zero()}; /**< Fixed sampling time in
+                                                                        seconds */
     EstimatedContact m_dummyContact; /**< A dummy esitmated contact */
 
     /**
@@ -83,9 +86,9 @@ public:
      * Initialize the detector.
      * @param handler pointer to the parameter handler.
      * @note the following parameters are required by the class
-     * |   Parameter Name  |    Type    |                Description                 | Mandatory |
-     * |:-----------------:|:----------:|:------------------------------------------:|:---------:|
-     * |  `sampling_time`  |  `double`  |  Sampling time of the detector is seconds  |    Yes    |
+     * |   Parameter Name  |           Type        |                Description                 | Mandatory |
+     * |:-----------------:|:---------------------:|:------------------------------------------:|:---------:|
+     * |  `sampling_time`  | `chrono::nanoseconds` |  Sampling time of the detector is seconds  |    Yes    |
      * @return true in case of success/false otherwise.
      */
     bool initialize(std::weak_ptr<const ParametersHandler::IParametersHandler> handler) override;
@@ -104,9 +107,9 @@ public:
 
     /**
      * Reset the time
-     * @param time the time in seconds
+     * @param time time
      */
-    void resetTime(const double& time);
+    void resetTime(const std::chrono::nanoseconds& time);
 
     /**
      * Get the fixed foot

src/Contacts/src/ContactDetector.cpp
@@ -7,6 +7,7 @@
 
 #include <BipedalLocomotion/ContactDetectors/ContactDetector.h>
 #include <BipedalLocomotion/TextLogging/Logger.h>
+#include <chrono>
 
 using namespace BipedalLocomotion::ParametersHandler;
 using namespace BipedalLocomotion::Contacts;
@@ -15,7 +16,7 @@ bool ContactDetector::resetContacts()
 {
     for (auto& [name, contact] : m_contactStates)
     {
-        contact.switchTime = 0.0;
+        contact.switchTime = std::chrono::nanoseconds::zero();
         contact.isActive = false;
     }
     return true;

src/Contacts/src/FixedFootDetector.cpp
@@ -10,6 +10,7 @@
 #include <BipedalLocomotion/ParametersHandler/IParametersHandler.h>
 #include <BipedalLocomotion/TextLogging/Logger.h>
 
+#include <chrono>
 #include <limits>
 #include <memory>
 
@@ -39,7 +40,8 @@ bool FixedFootDetector::initialize(std::weak_ptr<const IParametersHandler> handl
         return false;
     }
 
-    if (m_dT <= 0)
+    // This should never happen
+    if (m_dT <= std::chrono::nanoseconds::zero())
     {
         log()->error("{} The parameter 'sampling_time' must be a strictly positive number.",
                      logPrefix);
@@ -243,7 +245,7 @@ void FixedFootDetector::setContactPhaseList(const ContactPhaseList& phaseList)
     }
 }
 
-void FixedFootDetector::resetTime(const double &time)
+void FixedFootDetector::resetTime(const std::chrono::nanoseconds &time)
 {
     m_currentTime = time;
 }

src/Contacts/src/SchmittTriggerDetector.cpp
@@ -10,6 +10,7 @@
 #include <BipedalLocomotion/Math/SchmittTrigger.h>
 #include <BipedalLocomotion/ParametersHandler/IParametersHandler.h>
 #include <BipedalLocomotion/TextLogging/Logger.h>
+#include <chrono>
 
 namespace blf = BipedalLocomotion;
 using namespace BipedalLocomotion::Contacts;
@@ -73,10 +74,10 @@ bool SchmittTriggerDetector::initialize(std::weak_ptr<const IParametersHandler> 
     std::vector<double> offThreshold;
     ok = ok && setupParam("contact_break_thresholds", offThreshold);
 
-    std::vector<double> switchOnAfter;
+    std::vector<std::chrono::nanoseconds> switchOnAfter;
     ok = ok && setupParam("contact_make_switch_times", switchOnAfter);
 
-    std::vector<double> switchOffAfter;
+    std::vector<std::chrono::nanoseconds> switchOffAfter;
     ok = ok && setupParam("contact_break_switch_times", switchOffAfter);
 
     if (!ok)
@@ -101,7 +102,9 @@ bool SchmittTriggerDetector::initialize(std::weak_ptr<const IParametersHandler> 
         params.offThreshold = offThreshold[idx];
 
         // set the initial state for the trigger
-        constexpr blf::Math::SchmittTriggerState initialState{false, 0, 0};
+        constexpr blf::Math::SchmittTriggerState initialState{false,
+                                                              std::chrono::nanoseconds::zero(),
+                                                              std::chrono::nanoseconds::zero()};
         if (!this->addContact(contacts[idx], initialState, params))
         {
             log()->error("{} Could not add Schmitt Trigger unit for specified contact.", logPrefix);
@@ -262,7 +265,7 @@ bool SchmittTriggerDetector::resetContact(const std::string& contactName,
     triggerState.state = state;
     trigger.setState(triggerState);
     m_contactStates.at(contactName).isActive = state;
-    m_contactStates.at(contactName).switchTime = 0.0;
+    m_contactStates.at(contactName).switchTime = std::chrono::nanoseconds::zero();
 
     return true;
 }
@@ -280,7 +283,7 @@ bool SchmittTriggerDetector::resetState(const std::string& contactName, const bo
     triggerState.state = state;
     m_pimpl->manager.at(contactName).setState(triggerState);
     m_contactStates.at(contactName).isActive = state;
-    m_contactStates.at(contactName).switchTime = 0.0;
+    m_contactStates.at(contactName).switchTime = std::chrono::nanoseconds::zero();
 
     return true;
 }

src/Contacts/tests/ContactDetectors/FixedFootDetectorUnitTest.cpp
@@ -12,6 +12,7 @@
 #include <BipedalLocomotion/ContactDetectors/FixedFootDetector.h>
 #include <BipedalLocomotion/ParametersHandler/IParametersHandler.h>
 #include <BipedalLocomotion/ParametersHandler/StdImplementation.h>
+#include <chrono>
 
 using namespace BipedalLocomotion::ParametersHandler;
 using namespace BipedalLocomotion::Contacts;
@@ -22,46 +23,48 @@ struct FixedFootState
     EstimatedContact rightFoot;
 };
 
-FixedFootState getFixedFootState(double t, const ContactListMap& listMap)
+FixedFootState getFixedFootState(const std::chrono::nanoseconds& t, const ContactListMap& listMap)
 {
     // t            0   1   2   3   4   5   6   7   8   9   10  11  12  13  14  15  16  17
     // L            |+++|---|+++++++++++|---|+++++++++++|---|+++++++++++|---|+++++++++++|
     // R            |+++++++++++|---|+++++++++++|---|+++++++++++|---|+++++++++++|---|+++|
     // stance foot  |LLL|RRR|RRR|LLL|LLL|RRR|RRR|LLL|LLL|RRR|RRR|LLL|LLL|RRR|RRR|LLL|LLL|
 
+    using namespace std::chrono_literals;
+
     FixedFootState state;
     state.leftFoot.pose = listMap.find("left_foot")->second.getPresentContact(t)->pose;
     state.rightFoot.pose = listMap.find("right_foot")->second.getPresentContact(t)->pose;
 
-    if (t < 1)
+    if (t < 1s)
     {
         state.leftFoot.isActive = true;
         state.rightFoot.isActive = false;
-    } else if (t < 3)
+    } else if (t < 3s)
     {
         state.leftFoot.isActive = false;
         state.rightFoot.isActive = true;
-    } else if (t < 5)
+    } else if (t < 5s)
     {
         state.leftFoot.isActive = true;
         state.rightFoot.isActive = false;
-    } else if (t < 7)
+    } else if (t < 7s)
     {
         state.leftFoot.isActive = false;
         state.rightFoot.isActive = true;
-    } else if (t < 9)
+    } else if (t < 9s)
     {
         state.leftFoot.isActive = true;
         state.rightFoot.isActive = false;
-    } else if (t < 11)
+    } else if (t < 11s)
     {
         state.leftFoot.isActive = false;
         state.rightFoot.isActive = true;
-    } else if (t < 13)
+    } else if (t < 13s)
     {
         state.leftFoot.isActive = true;
         state.rightFoot.isActive = false;
-    } else if (t < 15)
+    } else if (t < 15s)
     {
         state.leftFoot.isActive = false;
         state.rightFoot.isActive = true;
@@ -82,49 +85,50 @@ ContactPhaseList createContactList()
     // stance foot  |LLL|RRR|RRR|LLL|LLL|RRR|RRR|LLL|LLL|RRR|RRR|LLL|LLL|RRR|RRR|LLL|LLL|
 
     ContactListMap contactListMap;
+    using namespace std::chrono_literals;
 
     Eigen::Vector3d leftPosition = Eigen::Vector3d::Zero();
     manif::SE3d leftTransform(leftPosition, manif::SO3d::Identity());
-    contactListMap["left_foot"].addContact(leftTransform, 0.0, 1.0);
+    contactListMap["left_foot"].addContact(leftTransform, 0s, 1s);
 
     leftPosition(0) += 0.05;
     leftTransform.translation(leftPosition);
-    contactListMap["left_foot"].addContact(leftTransform, 2.0, 5.0);
+    contactListMap["left_foot"].addContact(leftTransform, 2s, 5s);
 
     leftPosition(0) += 0.1;
     leftTransform.translation(leftPosition);
-    contactListMap["left_foot"].addContact(leftTransform, 6.0, 9.0);
+    contactListMap["left_foot"].addContact(leftTransform, 6s, 9s);
 
     leftPosition(0) += 0.1;
     leftTransform.translation(leftPosition);
-    contactListMap["left_foot"].addContact(leftTransform, 10.0, 13.0);
+    contactListMap["left_foot"].addContact(leftTransform, 10s, 13s);
 
     leftPosition(0) += 0.1;
     leftTransform.translation(leftPosition);
-    contactListMap["left_foot"].addContact(leftTransform, 14.0, 17.0);
+    contactListMap["left_foot"].addContact(leftTransform, 14s, 17s);
 
     // right foot
     // first footstep
     Eigen::Vector3d rightPosition = Eigen::Vector3d::Zero();
     manif::SE3d rightTransform(rightPosition, manif::SO3d::Identity());
 
-    contactListMap["right_foot"].addContact(rightTransform, 0.0, 3.0);
+    contactListMap["right_foot"].addContact(rightTransform, 0s, 3s);
 
     rightPosition(0) += 0.1;
     rightTransform.translation(rightPosition);
-    contactListMap["right_foot"].addContact(rightTransform, 4.0, 7.0);
+    contactListMap["right_foot"].addContact(rightTransform, 4s, 7s);
 
     rightPosition(0) += 0.1;
     rightTransform.translation(rightPosition);
-    contactListMap["right_foot"].addContact(rightTransform, 8.0, 11.0);
+    contactListMap["right_foot"].addContact(rightTransform, 8s, 11s);
 
     rightPosition(0) += 0.1;
     rightTransform.translation(rightPosition);
-    contactListMap["right_foot"].addContact(rightTransform, 12.0, 15.0);
+    contactListMap["right_foot"].addContact(rightTransform, 12s, 15s);
 
     rightPosition(0) += 0.05;
     rightTransform.translation(rightPosition);
-    contactListMap["right_foot"].addContact(rightTransform, 16.0, 17.0);
+    contactListMap["right_foot"].addContact(rightTransform, 16s, 17s);
 
     ContactPhaseList phaseList;
     phaseList.setLists(contactListMap);
@@ -133,8 +137,9 @@ ContactPhaseList createContactList()
 
 TEST_CASE("Fixed Foot Detector")
 {
-    constexpr auto dT = 0.01;
-    constexpr auto horizon = 20.0;
+    using namespace std::chrono_literals;
+    constexpr std::chrono::nanoseconds dT = 10ms;
+    constexpr std::chrono::nanoseconds horizon = 20s;
     FixedFootDetector detector;
     auto handler = std::make_shared<StdImplementation>();
     handler->setParameter("sampling_time", dT);
@@ -143,12 +148,11 @@ TEST_CASE("Fixed Foot Detector")
     const auto phaseList = createContactList();
     detector.setContactPhaseList(phaseList);
 
-    for (int i = 0; i < horizon / dT; i++)
+    for (std::chrono::nanoseconds currentTime = 0s; currentTime < horizon; currentTime += dT)
     {
         // advance is used to advance the time stored in the detector and to evaluate the outputs
         REQUIRE(detector.advance());
 
-        const double currentTime = phaseList.firstPhase()->beginTime + i * dT;
         auto state = getFixedFootState(currentTime, phaseList.lists());
 
         REQUIRE(detector.getOutput().find("right_foot")->second.isActive == state.rightFoot.isActive);

src/Contacts/tests/ContactDetectors/SchmittTriggerDetectorUnitTest.cpp
@@ -5,6 +5,8 @@
  * distributed under the terms of the BSD-3-Clause license.
  */
 
+#include <chrono>
+
 #include <catch2/catch.hpp>
 
 #include <BipedalLocomotion/ContactDetectors/SchmittTriggerDetector.h>
@@ -20,85 +22,90 @@ TEST_CASE("Schmitt Trigger Detector")
 {
     SchmittTriggerDetector detector;
 
+    using namespace std::chrono_literals;
+
     std::shared_ptr<StdImplementation> originalHandler = std::make_shared<StdImplementation>();
     IParametersHandler::shared_ptr parameterHandler = originalHandler;
 
     parameterHandler->setParameter("contacts", std::vector<std::string>{"right"});
     parameterHandler->setParameter("contact_make_thresholds", std::vector<double>{100});
     parameterHandler->setParameter("contact_break_thresholds", std::vector<double>{10});
-    parameterHandler->setParameter("contact_make_switch_times", std::vector<double>{0.2});
-    parameterHandler->setParameter("contact_break_switch_times", std::vector<double>{0.2, 0.2});
+    parameterHandler->setParameter("contact_make_switch_times",
+                                   std::vector<std::chrono::nanoseconds>{200ms});
+    parameterHandler->setParameter("contact_break_switch_times",
+                                   std::vector<std::chrono::nanoseconds>{200ms, 200ms});
 
     // mismatch in parameter size shoulc cause initialization failure
     REQUIRE_FALSE(detector.initialize(parameterHandler));
 
     // initialization should pass
-    parameterHandler->setParameter("contact_break_switch_times", std::vector<double>{0.2});
+    parameterHandler->setParameter("contact_break_switch_times",
+                                   std::vector<std::chrono::nanoseconds>{200ms});
     REQUIRE(detector.initialize(parameterHandler));
 
     // set all contacts to false
     REQUIRE(detector.resetContacts());
 
     // rise signal
-    detector.setTimedTriggerInput("right", {0.1 ,120});
+    detector.setTimedTriggerInput("right", {100ms ,120});
     detector.advance();
-    detector.setTimedTriggerInput("right", {0.2, 120});
+    detector.setTimedTriggerInput("right", {200ms, 120});
     detector.advance();
-    detector.setTimedTriggerInput("right", {0.3, 120});
+    detector.setTimedTriggerInput("right", {300ms, 120});
     detector.advance();
 
     // contact state should turn true
     EstimatedContact rightContact;
     REQUIRE(detector.get("right", rightContact));
     REQUIRE(rightContact.isActive);
-    REQUIRE(rightContact.switchTime == 0.3);
+    REQUIRE(rightContact.switchTime == 300ms);
 
     // fall signal
-    detector.setTimedTriggerInput("right", {0.4, 7});
+    detector.setTimedTriggerInput("right", {400ms, 7});
     detector.advance();
-    detector.setTimedTriggerInput("right", {0.5, 7});
+    detector.setTimedTriggerInput("right", {500ms, 7});
     detector.advance();
-    detector.setTimedTriggerInput("right", {0.6, 7});
+    detector.setTimedTriggerInput("right", {600ms, 7});
     detector.advance();
 
     // contact state should turn false
     REQUIRE(detector.get("right", rightContact));
     REQUIRE(!rightContact.isActive);
-    REQUIRE(rightContact.switchTime == 0.6);
+    REQUIRE(rightContact.switchTime == 600ms);
 
     // add a new contact
     EstimatedContact newContact;
     SchmittTrigger::Params params;
     params.offThreshold = 10;
     params.onThreshold = 100;
-    params.switchOffAfter = 0.2;
-    params.switchOnAfter = 0.2;
+    params.switchOffAfter = 200ms;
+    params.switchOnAfter = 200ms;
 
-    detector.addContact("left", {false, 0.6, 0.6}, params);
+    detector.addContact("left", {false, 600ms, 600ms}, params);
     auto contacts = detector.getOutput();
     REQUIRE(contacts.size() == 2);
 
     // test multiple measurement updates
     std::unordered_map<std::string, SchmittTriggerInput> timedForces;
-    timedForces["right"] = {0.7, 120};
-    timedForces["left"] = {0.7, 120};
+    timedForces["right"] = {700ms, 120};
+    timedForces["left"] = {700ms, 120};
     detector.setTimedTriggerInputs(timedForces);
     detector.advance();
 
-    timedForces["right"] = {0.8, 120};
-    timedForces["left"] = {0.8, 120};
+    timedForces["right"] = {800ms, 120};
+    timedForces["left"] = {800ms, 120};
     detector.setTimedTriggerInputs(timedForces);
     detector.advance();
 
-    timedForces["right"] = {0.9, 120};
-    timedForces["left"] = {0.9, 120};
+    timedForces["right"] = {900ms, 120};
+    timedForces["left"] = {900ms, 120};
     detector.setTimedTriggerInputs(timedForces);
     detector.advance();
 
     contacts = detector.getOutput();
     REQUIRE(contacts["right"].isActive);
     REQUIRE(contacts["left"].isActive);
-    REQUIRE(contacts["right"].switchTime == 0.9);
+    REQUIRE(contacts["right"].switchTime == 900ms);
 
     // Test removing contact
     REQUIRE(detector.removeContact("left"));

commit b91fb10341c858aba6a55df1401cf5fdc570323e

Fix regression in test_fixed_foot_detector.py
Regression caused by 95a29b4089a34aa294a2107f867ac1db20b78059

bindings/python/Contacts/tests/test_fixed_foot_detector.py
@@ -6,116 +6,166 @@ import bipedal_locomotion_framework.bindings as blf
 import manifpy as manif
 import numpy as np
 
+from datetime import timedelta
+from typing import Dict
 
-def test_fixed_foot_detector():
-    dt = 0.01
+class FixedFootState:
+    def __init__(self):
+        self.left_foot = blf.contacts.EstimatedContact()
+        self.right_foot = blf.contacts.EstimatedContact()
 
-    parameters_handler = blf.parameters_handler.StdParametersHandler()
-    parameters_handler.set_parameter_float("sampling_time", dt)
+def get_fixed_foot_state(t: timedelta, list_map: Dict[str, blf.contacts.ContactList]) -> FixedFootState:
 
-    detector = blf.contacts.FixedFootDetector()
-    assert (detector.initialize(parameters_handler))
+    # t            0   1   2   3   4   5   6   7   8   9   10  11  12  13  14  15  16  17
+    # L            |+++|---|+++++++++++|---|+++++++++++|---|+++++++++++|---|+++++++++++|
+    # R            |+++++++++++|---|+++++++++++|---|+++++++++++|---|+++++++++++|---|+++|
+    # stance foot  |LLL|RRR|RRR|LLL|LLL|RRR|RRR|LLL|LLL|RRR|RRR|LLL|LLL|RRR|RRR|LLL|LLL|
+    state = FixedFootState()
+    state.left_foot.pose = list_map["left_foot"].get_present_contact(t).pose
+    state.right_foot.pose = list_map["right_foot"].get_present_contact(t).pose
 
-    # Create the map of contact lists
+    if t < timedelta(seconds=1):
+        state.left_foot.is_active = True
+        state.right_foot.is_active = False
+    elif t < timedelta(seconds=3):
+        state.left_foot.is_active = False
+        state.right_foot.is_active = True
+    elif t < timedelta(seconds=5):
+        state.left_foot.is_active = True
+        state.right_foot.is_active = False
+    elif t < timedelta(seconds=7):
+        state.left_foot.is_active = False
+        state.right_foot.is_active = True
+    elif t < timedelta(seconds=9):
+        state.left_foot.is_active = True
+        state.right_foot.is_active = False
+    elif t < timedelta(seconds=11):
+        state.left_foot.is_active = False
+        state.right_foot.is_active = True
+    elif t < timedelta(seconds=13):
+        state.left_foot.is_active = True
+        state.right_foot.is_active = False
+    elif t < timedelta(seconds=15):
+        state.left_foot.is_active = False
+        state.right_foot.is_active = True
+    else:
+        state.left_foot.is_active = True
+        state.right_foot.is_active = False
+
+    return state
+
+def create_contact_list() -> blf.contacts.ContactPhaseList:
+    # t            0   1   2   3   4   5   6   7   8   9   10  11  12  13  14  15  16  17
+    # L            |+++|---|+++++++++++|---|+++++++++++|---|+++++++++++|---|+++++++++++|
+    # R            |+++++++++++|---|+++++++++++|---|+++++++++++|---|+++++++++++|---|+++|
+    # stance foot  |LLL|RRR|RRR|LLL|LLL|RRR|RRR|LLL|LLL|RRR|RRR|LLL|LLL|RRR|RRR|LLL|LLL|
+
     contact_list_map = dict()
+    contact_list_map["left_foot"] = blf.contacts.ContactList()
+    contact_list_map["right_foot"] = blf.contacts.ContactList()
 
-    # Names of the frames
-    rfoot_frame = "r_sole"
-    lfoot_frame = "l_sole"
+    left_position = np.array([0.0, 0.08, 0.0])
+    # quaternion=[0, 0, 0, 1] please read it as 0i + 0j + 0k + 1
+    left_transform = manif.SE3(left_position, [0, 0, 0, 1])
+    assert(contact_list_map["left_foot"].add_contact(left_transform,
+                                                      timedelta(seconds=0),
+                                                      timedelta(seconds=1)))
 
-    # Create the contact lists
-    contact_list_map[rfoot_frame] = blf.contacts.ContactList()
-    contact_list_map[lfoot_frame] = blf.contacts.ContactList()
+    left_position[0] += 0.05
+    left_transform.translation(left_position)
+    assert(contact_list_map["left_foot"].add_contact(left_transform,
+                                                     timedelta(seconds=2),
+                                                     timedelta(seconds=5)))
 
-    # Set the contacts (t: time, L: left foot, R: right foot)
+    left_position[0] += 0.1
+    left_transform.translation(left_position)
+    assert(contact_list_map["left_foot"].add_contact(left_transform,
+                                                     timedelta(seconds=6),
+                                                     timedelta(seconds=9)))
 
-    # t:   ||    1    2    3    4    5    6    7    8    9    10
-    #      ||----|----|----|----|----|----|----|----|----|----||
-    # L:   ||****|****|****|    |****|****|****|****|****|****||
-    # R:   ||****|****|****|****|****|****|    |****|****|****||
+    left_position[0] += 0.1
+    left_transform.translation(left_position)
+    assert(contact_list_map["left_foot"].add_contact(left_transform,
+                                                     timedelta(seconds=10),
+                                                     timedelta(seconds=13)))
 
-    # Initial foot position and orientation
-    r_foot_quat = [1, 0, 0, 0]
-    r_foot_position = [0, 0.7, 0]
-    l_foot_quat = [1, 0, 0, 0]
-    l_foot_position = [0, -0.7, 0]
+    left_position[0] += 0.1
+    left_transform.translation(left_position)
+    assert(contact_list_map["left_foot"].add_contact(left_transform,
+                                                     timedelta(seconds=14),
+                                                     timedelta(seconds=17)))
 
-    assert contact_list_map[lfoot_frame].add_contact(
-        transform=manif.SE3(position=np.array(l_foot_position),
-                            quaternion=l_foot_quat),
-        activation_time=0.0,
-        deactivation_time=3.0)
-    assert contact_list_map[rfoot_frame].add_contact(
-        transform=manif.SE3(position=np.array(r_foot_position),
-                            quaternion=r_foot_quat),
-        activation_time=0.0,
-        deactivation_time=6.0)
 
-    assert contact_list_map[lfoot_frame].add_contact(
-        transform=manif.SE3(position=np.array([l_foot_position[0] + 0.1, l_foot_position[1], 0.0]),
-                            quaternion=l_foot_quat),
-        activation_time=4.0,
-        deactivation_time=10.0)
-    assert contact_list_map[rfoot_frame].add_contact(
-        transform=manif.SE3(position=np.array([r_foot_position[0] + 0.2, r_foot_position[1], 0.0]),
-                            quaternion=r_foot_quat),
-        activation_time=7.0,
-        deactivation_time=10.0)
+    right_position = np.array([0.0, -0.08, 0.0])
+    # quaternion=[0, 0, 0, 1] please read it as 0i + 0j + 0k + 1
+    right_transform = manif.SE3(right_position, [0, 0, 0, 1])
+
+    assert(contact_list_map["right_foot"].add_contact(right_transform,
+                                                      timedelta(seconds=0),
+                                                      timedelta(seconds=3)))
+
+    right_position[0] += 0.1
+    right_transform.translation(right_position)
+    assert(contact_list_map["right_foot"].add_contact(right_transform,
+                                                      timedelta(seconds=4),
+                                                      timedelta(seconds=7)))
+
+    right_position[0] += 0.1
+    right_transform.translation(right_position)
+    assert(contact_list_map["right_foot"].add_contact(right_transform,
+                                                      timedelta(seconds=8),
+                                                      timedelta(seconds=11)))
+
+    right_position[0] += 0.1
+    right_transform.translation(right_position)
+    assert(contact_list_map["right_foot"].add_contact(right_transform,
+                                                      timedelta(seconds=12),
+                                                      timedelta(seconds=15)))
+
+    right_position[0] += 0.05
+    right_transform.translation(right_position)
+    assert(contact_list_map["right_foot"].add_contact(right_transform,
+                                                      timedelta(seconds=16),
+                                                      timedelta(seconds=17)))
+
 
     phase_list = blf.contacts.ContactPhaseList()
-    phase_list.set_lists(contact_lists=contact_list_map)
+    phase_list.set_lists(contact_list_map)
 
-    assert (detector.set_contact_phase_list(phase_list))
+    return phase_list
 
-    # Retrieve initial fixed foot
-    fixed_foot = detector.get_fixed_foot()
 
-    assert (fixed_foot.name == "l_sole")
-    assert (fixed_foot.pose == manif.SE3(position=np.array(l_foot_position), quaternion=l_foot_quat))
-    assert (fixed_foot.switch_time == 0.0)
-    assert (fixed_foot.last_update_time == 0.0)
+def time_generator(start: timedelta, stop: timedelta, step: timedelta):
+  while start < stop:
+    yield start
+    start = start + step
 
-    # Retrieve fixed foot along the trajectory
-    for i in np.arange(start=dt, step=dt, stop=10.0):
+def test_fixed_foot_detector():
+    dt = timedelta(milliseconds=10)
+    horizon = timedelta(seconds=20)
 
-        assert (detector.advance())
-        fixed_foot = detector.get_fixed_foot()
+    detector = blf.contacts.FixedFootDetector()
+    parameters_handler = blf.parameters_handler.StdParametersHandler()
+    parameters_handler.set_parameter_datetime("sampling_time", dt)
 
-        # Fixed foot: left - Phase: double support
-        if i <= 3.0:
-            assert (fixed_foot.name == "l_sole")
-            assert (fixed_foot.pose == manif.SE3(position=np.array(l_foot_position), quaternion=l_foot_quat))
-            assert (fixed_foot.switch_time == 0.0)
-            assert (round(fixed_foot.last_update_time, 2) == round(i, 2)) # last_update_time updated incrementally
+    assert (detector.initialize(parameters_handler))
 
-        # Fixed foot: right - Phase: single support
-        elif i <= 4.0:
-            assert (fixed_foot.name == "r_sole")
-            assert (fixed_foot.pose == manif.SE3(position=np.array(r_foot_position), quaternion=r_foot_quat))
-            assert (fixed_foot.switch_time == 3.0)
-            assert (round(fixed_foot.last_update_time, 2) == round(i, 2)) # last_update_time updated incrementally
+    phase_list = create_contact_list()
+    detector.set_contact_phase_list(phase_list)
 
-        # Fixed foot: right - Phase: double support
-        elif i <= 6.0:
-            assert (fixed_foot.name == "r_sole")
-            assert (fixed_foot.pose == manif.SE3(position=np.array(r_foot_position), quaternion=r_foot_quat))
-            assert (fixed_foot.switch_time == 3.0)
-            assert (round(fixed_foot.last_update_time, 2) == 4.0) # last_update_time fixed to the beginning of DS
+    for current_time in time_generator(timedelta(seconds=0), horizon, dt):
 
-        # Fixed foot: left - Phase: single support
-        elif i <= 7.0:
-            assert (fixed_foot.name == "l_sole")
-            assert (fixed_foot.pose == manif.SE3(
-                position=np.array(np.array([l_foot_position[0] + 0.1, l_foot_position[1], 0.0])),
-                quaternion=l_foot_quat))
-            assert (fixed_foot.switch_time == 6.0)
-            assert (round(fixed_foot.last_update_time, 2) == round(i, 2)) # last_update_time updated incrementally
+        assert(detector.advance())
+        assert(detector.is_output_valid())
+        output = detector.get_output()
 
-        # Fixed foot: left - Phase: double support
-        elif i <= 10.0:
-            assert (fixed_foot.name == "l_sole")
-            assert (fixed_foot.pose == manif.SE3(
-                position=np.array(np.array([l_foot_position[0] + 0.1, l_foot_position[1], 0.0])),
-                quaternion=l_foot_quat))
-            assert (fixed_foot.switch_time == 6.0)
-            assert (round(fixed_foot.last_update_time, 2) == 7.0) # last_update_time fixed to the beginning of DS
+        state = get_fixed_foot_state(current_time, phase_list.lists())
+        assert(output["left_foot"].is_active == state.left_foot.is_active)
+        assert(output["right_foot"].is_active == state.right_foot.is_active)
+
+        if state.left_foot.is_active:
+            assert(output["left_foot"].pose == state.left_foot.pose)
+        elif state.right_foot.is_active:
+            assert(output["right_foot"].pose == state.right_foot.pose)
+        else:
+            assert(False) # This should never happen