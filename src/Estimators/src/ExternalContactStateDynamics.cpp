7005833558f3478a15eeeee94a0994b8aa522eb6
Ines <ines.sorrentino10@gmail.com>
2023-11-10 15:45:45 +0100

    Add process dynamics class for external contacts

---
+++ src/Estimators/src/ExternalContactStateDynamics.cpp
@@ -0,0 +1,148 @@
+/**
+ * @file ExternalContactStateDynamics.cpp
+ * @authors Ines Sorrentino
+ * @copyright 2023 Istituto Italiano di Tecnologia (IIT). This software may be modified and
+ * distributed under the terms of the BSD-3-Clause license.
+ */
+
+#include <BipedalLocomotion/RobotDynamicsEstimator/ExternalContactStateDynamics.h>
+#include <BipedalLocomotion/TextLogging/Logger.h>
+
+namespace RDE = BipedalLocomotion::Estimators::RobotDynamicsEstimator;
+
+bool RDE::ExternalContactStateDynamics::initialize(
+    std::weak_ptr<const ParametersHandler::IParametersHandler> paramHandler)
+{
+    constexpr auto errorPrefix = "[ExternalContactStateDynamics::initialize]";
+
+    auto ptr = paramHandler.lock();
+    if (ptr == nullptr)
+    {
+        log()->error("{} The parameter handler is not valid.", errorPrefix);
+        return false;
+    }
+
+    // Set the state dynamics name
+    if (!ptr->getParameter("name", m_name))
+    {
+        log()->error("{} Error while retrieving the name variable.", errorPrefix);
+        return false;
+    }
+
+    // Set the state process covariance
+    if (!ptr->getParameter("covariance", m_covariances))
+    {
+        log()->error("{} Error while retrieving the covariance variable.", errorPrefix);
+        return false;
+    }
+
+    // Set the state initial covariance
+    if (!ptr->getParameter("initial_covariance", m_initialCovariances))
+    {
+        log()->error("{} Variable initial_covariance not found.", errorPrefix);
+        return false;
+    }
+
+    // Set the list of elements if it exists
+    if (!ptr->getParameter("elements", m_elements))
+    {
+        log()->debug("{} Variable elements not found.", errorPrefix);
+    }
+
+    if (!ptr->getParameter("sampling_time", m_dT))
+    {
+        log()->error("{} Error while retrieving the sampling_time variable.", errorPrefix);
+        return false;
+    }
+
+    if (!ptr->getParameter("k", m_k))
+    {
+        log()->error("{} Error while retrieving the friction_k2 variable.", errorPrefix);
+        return false;
+    }
+
+    m_description = "Zero velocity dynamics";
+
+    m_isInitialized = true;
+
+    return true;
+}
+
+bool RDE::ExternalContactStateDynamics::finalize(
+    const System::VariablesHandler& stateVariableHandler)
+{
+    constexpr auto errorPrefix = "[ExternalContactStateDynamics::finalize]";
+
+    if (!m_isInitialized)
+    {
+        log()->error("{} Please initialize the dynamics before calling finalize.", errorPrefix);
+        return false;
+    }
+
+    if (stateVariableHandler.getNumberOfVariables() == 0)
+    {
+        log()->error("{} The state variable handler is empty.", errorPrefix);
+        return false;
+    }
+
+    m_stateVariableHandler = stateVariableHandler;
+
+    if (!checkStateVariableHandler())
+    {
+        log()->error("{} The state variable handler is not valid.", errorPrefix);
+        return false;
+    }
+
+    m_size = m_covariances.size();
+
+    m_currentState.resize(m_size);
+    m_currentState.setZero();
+
+    m_updatedVariable.resize(m_size);
+    m_updatedVariable.setZero();
+
+    return true;
+}
+
+bool RDE::ExternalContactStateDynamics::setSubModels(
+    const std::vector<RDE::SubModel>& /*subModelList*/,
+    const std::vector<std::shared_ptr<RDE::KinDynWrapper>>& /*kinDynWrapperList*/)
+{
+    return true;
+}
+
+bool RDE::ExternalContactStateDynamics::checkStateVariableHandler()
+{
+    constexpr auto errorPrefix = "[ExternalContactStateDynamics::checkStateVariableHandler]";
+
+    // Check if the variable handler contains the variables used by this dynamics
+    if (!m_stateVariableHandler.getVariable(m_name).isValid())
+    {
+        log()->error("{} The variable handler does not contain the expected state with name `{}`.",
+                     errorPrefix,
+                     m_name);
+        return false;
+    }
+
+    return true;
+}
+
+bool RDE::ExternalContactStateDynamics::update()
+{
+    m_updatedVariable = m_k.asDiagonal() * m_currentState;
+    m_updatedVariable *= -std::chrono::duration<double>(m_dT).count();
+    m_updatedVariable += m_currentState;
+
+    return true;
+}
+
+void RDE::ExternalContactStateDynamics::setState(const Eigen::Ref<const Eigen::VectorXd> ukfState)
+{
+    m_currentState = ukfState.segment(m_stateVariableHandler.getVariable(m_name).offset,
+                                      m_stateVariableHandler.getVariable(m_name).size);
+}
+
+void RDE::ExternalContactStateDynamics::setInput(const UKFInput& /*ukfInput*/)
+{
+    return;
+}
---
+++ src/Estimators/tests/RobotDynamicsEstimator/ExternalContactStateDynamicsTest.cpp
@@ -0,0 +1,81 @@
+/**
+ * @file ZeroVelocityStateDynamicsTest.cpp
+ * @authors Ines Sorrentino
+ * @copyright 2023 Istituto Italiano di Tecnologia (IIT). This software may be modified and
+ * distributed under the terms of the BSD-3-Clause license.
+ */
+
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators_all.hpp>
+#include <chrono>
+
+#include <yarp/os/ResourceFinder.h>
+
+#include <BipedalLocomotion/ParametersHandler/StdImplementation.h>
+
+#include <BipedalLocomotion/RobotDynamicsEstimator/ExternalContactStateDynamics.h>
+
+using namespace BipedalLocomotion::Estimators::RobotDynamicsEstimator;
+using namespace BipedalLocomotion::ParametersHandler;
+using namespace BipedalLocomotion::System;
+
+TEST_CASE("External Contact State Dynamics")
+{
+    auto parameterHandler = std::make_shared<StdImplementation>();
+
+    // Test without bias
+    const std::string name = "r_sole";
+    Eigen::VectorXd covariance(6);
+    covariance << 1e-3, 1e-3, 5e-3, 5e-3, 5e-3, 5e-3;
+    const std::string model = "ExternalContactStateDynamics";
+    Eigen::VectorXd k(6);
+    k << 1e-1, 1e-1, 1e-1, 1e-1, 1e-1, 1e-1;
+    using namespace std::chrono_literals;
+    constexpr std::chrono::nanoseconds dT = 10ms;
+
+    parameterHandler->setParameter("name", name);
+    parameterHandler->setParameter("covariance", covariance);
+    parameterHandler->setParameter("initial_covariance", covariance);
+    parameterHandler->setParameter("dynamic_model", model);
+    parameterHandler->setParameter("sampling_time", dT);
+    parameterHandler->setParameter("k", k);
+
+    // Create variable handler
+    constexpr size_t sizeVariable = 6;
+    VariablesHandler variableHandler;
+    REQUIRE(variableHandler.addVariable("ds", sizeVariable));
+    REQUIRE(variableHandler.addVariable("tau_m", sizeVariable));
+    REQUIRE(variableHandler.addVariable("tau_F", sizeVariable));
+    REQUIRE(variableHandler.addVariable("r_sole", 6));
+
+    ExternalContactStateDynamics r_sole_contact;
+
+    REQUIRE(r_sole_contact.initialize(parameterHandler));
+    REQUIRE(r_sole_contact.finalize(variableHandler));
+
+    Eigen::VectorXd currentState(covariance.size());
+    for (int index = 0; index < covariance.size(); index++)
+    {
+        currentState(index) = GENERATE(take(1, random(-100, 100)));
+    }
+
+    Eigen::VectorXd state(sizeVariable * variableHandler.getNumberOfVariables());
+    state.setZero();
+    state.segment(variableHandler.getVariable("r_sole").offset,
+                  variableHandler.getVariable("r_sole").size)
+        = currentState;
+
+    r_sole_contact.setState(state);
+
+    REQUIRE(r_sole_contact.update());
+
+    Eigen::VectorXd nextState(covariance.size());
+    nextState = r_sole_contact.getUpdatedVariable();
+
+    Eigen::VectorXd expectedNextState(covariance.size());
+    expectedNextState
+        = currentState.array()
+          - k.array() * currentState.array() * std::chrono::duration<double>(dT).count();
+
+    REQUIRE((expectedNextState - nextState).isZero());
+}
