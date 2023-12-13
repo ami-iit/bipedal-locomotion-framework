041d923eb0c39299c756f4433cd9e66243ec1960
Giulio Romualdi <giulio.romualdi@gmail.com>
2023-12-13 23:20:36 +0100

    Update src/Contacts/include/BipedalLocomotion/Contacts/ContactList.h
    Co-authored-by: Ines Sorrentino <43743081+isorrentino@users.noreply.github.com>

--- src/Contacts/include/BipedalLocomotion/Contacts/ContactList.h
+++ src/Contacts/include/BipedalLocomotion/Contacts/ContactList.h
@@ -211,7 +211,7 @@ public:
      * @brief Get the active contact given the time.
      *
      * It returns the active contact (i.e., the highest activation time lower than time and the
-     * deactivation time strictly higher than time) If no contacts is active at the given time, it
+     * deactivation time strictly higher than time). If no contacts are active at the given time, it
      * returns an iterator to the end.
      * @param time The present time.
      * @return an iterator to the last contact having an activation time lower than time.
