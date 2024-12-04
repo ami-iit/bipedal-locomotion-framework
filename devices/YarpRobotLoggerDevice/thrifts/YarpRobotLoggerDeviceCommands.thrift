e80cdad4288c7003bd0bb04f29b40f76c4793296
Lorenzo Moretti <107630048+LoreMoretti@users.noreply.github.com>
2024-12-04 09:04:36 +0100

    Update devices/YarpRobotLoggerDevice/thrifts/YarpRobotLoggerDeviceCommands.thrift
    Co-authored-by: Giulio Romualdi <giulio.romualdi@gmail.com>

--- devices/YarpRobotLoggerDevice/thrifts/YarpRobotLoggerDeviceCommands.thrift
+++ devices/YarpRobotLoggerDevice/thrifts/YarpRobotLoggerDeviceCommands.thrift
@@ -1,7 +1,7 @@
 /**
  * @file YarpRobotLoggerDeviceCommands.thrift
  * @authors Lorenzo Moretti <lorenzo.moretti@iit.it>
- * @copyright 2024 iCub Facility - Istituto Italiano di Tecnologia
+ * @copyright 2024 Artificial and Mechanical Intelligence - Istituto Italiano di Tecnologia
  *            Released under the terms of the BSD-3-Clause license.
  * @date 2024
  */
