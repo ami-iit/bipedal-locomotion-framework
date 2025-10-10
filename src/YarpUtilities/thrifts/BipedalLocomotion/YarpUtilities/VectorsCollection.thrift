93cab2ca4de1e2077767ce7596ca57498ba39c1f
Alessandro Croci <alessandro.croci@iit.it>
2025-10-10 18:02:13 +0200

    Add version field to VectorsCollection and VectorsCollectionMetadata structs

--- src/YarpUtilities/thrifts/BipedalLocomotion/YarpUtilities/VectorsCollection.thrift
+++ src/YarpUtilities/thrifts/BipedalLocomotion/YarpUtilities/VectorsCollection.thrift
@@ -3,11 +3,13 @@ namespace yarp BipedalLocomotion.YarpUtilities
 struct VectorsCollection
 {
     1: map<string, list<double>> vectors;
+    2: i32 version;
 }
 
 struct VectorsCollectionMetadata
 {
     1: map<string, list<string>> vectors;
+    2: i32 version;
 }
 
 service VectorsCollectionMetadataService
