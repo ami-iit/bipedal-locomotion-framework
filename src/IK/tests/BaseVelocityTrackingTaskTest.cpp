800697c396dc446fa0d4d470a1a64856c7ac0834
Davide <davidegorbani@gmail.com>
2025-10-16 11:56:59 +0200

    added a test for the BaseVelocityTrackingTask

---
+++ src/IK/tests/BaseVelocityTrackingTaskTest.cpp
@@ -0,0 +1,102 @@
+/**
+ * @file JointVelocityLimitsTaskTest.cpp
+ * @authors Davide Gorbani
+ * @copyright 2023 Istituto Italiano di Tecnologia (IIT). This software may be modified and
+ * distributed under the terms of the BSD-3-Clause license.
+ */
+
+// Catch2
+#include <catch2/catch_test_macros.hpp>
+
+// BipedalLocomotion
+#include <BipedalLocomotion/ParametersHandler/StdImplementation.h>
+#include <BipedalLocomotion/System/VariablesHandler.h>
+
+#include <BipedalLocomotion/IK/BaseVelocityTrackingTask.h>
+
+#include <iDynTree/EigenHelpers.h>
+#include <iDynTree/ModelTestUtils.h>
+
+#include <Eigen/Dense>
+
+using namespace BipedalLocomotion::ParametersHandler;
+using namespace BipedalLocomotion::System;
+using namespace BipedalLocomotion::IK;
+
+TEST_CASE("Base Velocity Tracking task")
+{
+    using namespace std::chrono_literals;
+    const std::string robotVelocity = "robotVelocity";
+
+    auto kinDyn = std::make_shared<iDynTree::KinDynComputations>();
+    auto parameterHandler = std::make_shared<StdImplementation>();
+
+    parameterHandler->setParameter("robot_velocity_variable_name", robotVelocity);
+
+    // set the velocity representation
+    REQUIRE(kinDyn->setFrameVelocityRepresentation(
+        iDynTree::FrameVelocityRepresentation::MIXED_REPRESENTATION));
+
+    for (std::size_t numberOfJoints = 6; numberOfJoints < 40; numberOfJoints += 15)
+    {
+        DYNAMIC_SECTION("Model with " << numberOfJoints << " joints")
+        {
+            // create the model
+            const iDynTree::Model model = iDynTree::getRandomModel(numberOfJoints);
+            REQUIRE(kinDyn->loadRobotModel(model));
+
+            const auto worldBasePos = iDynTree::getRandomTransform();
+            const auto baseVel = iDynTree::getRandomTwist();
+            iDynTree::VectorDynSize jointsPos(model.getNrOfDOFs());
+            iDynTree::VectorDynSize jointsVel(model.getNrOfDOFs());
+            iDynTree::Vector3 gravity;
+
+            for (auto& joint : jointsPos)
+            {
+                joint = iDynTree::getRandomDouble();
+            }
+
+            for (auto& joint : jointsVel)
+            {
+                joint = iDynTree::getRandomDouble();
+            }
+
+            for (auto& element : gravity)
+            {
+                element = iDynTree::getRandomDouble();
+            }
+
+            REQUIRE(kinDyn->setRobotState(worldBasePos, jointsPos, baseVel, jointsVel, gravity));
+
+            // Instantiate the handler
+            VariablesHandler variablesHandler;
+            variablesHandler.addVariable("dummy1", 10);
+            variablesHandler.addVariable(robotVelocity, model.getNrOfDOFs() + 6);
+            variablesHandler.addVariable("dummy2", 15);
+
+            Eigen::VectorXd desiredBaseVelocity(6);
+            desiredBaseVelocity.setZero();
+
+            BaseVelocityTrackingTask task;
+            REQUIRE(task.setKinDyn(kinDyn));
+            REQUIRE(task.initialize(parameterHandler));
+            REQUIRE(task.setVariablesHandler(variablesHandler));
+            REQUIRE(task.setSetPoint(desiredBaseVelocity));
+
+            REQUIRE(task.update());
+            REQUIRE(task.isValid());
+
+            // get A and b
+            Eigen::Ref<const Eigen::MatrixXd> A = task.getA();
+            Eigen::Ref<const Eigen::VectorXd> b = task.getB();
+
+            // check the matrix A
+            REQUIRE(A.block(0, 0, 6, 6).isApprox(Eigen::MatrixXd::Identity(6, 6)));
+
+            // check the vector b
+            Eigen::VectorXd expectedB;
+            expectedB = desiredBaseVelocity;
+            REQUIRE(b.head(6).isApprox(expectedB));
+        }
+    }
+}
